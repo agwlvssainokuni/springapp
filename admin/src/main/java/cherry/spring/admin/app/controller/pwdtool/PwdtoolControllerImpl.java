3512c9e61c2912f36a55b1da49562859047c30cb
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-02-08 17:34:06 +0900

    パスワードエンコーダを呼び出すツール。

---
+++ admin/src/main/java/cherry/spring/admin/app/controller/pwdtool/PwdtoolControllerImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2014 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.spring.admin.app.controller.pwdtool;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mobile.device.site.SitePreference;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * パスワードエンコーダツール。
+ */
+@Controller
+@RequestMapping(PwdtoolController.URI_PATH)
+public class PwdtoolControllerImpl implements PwdtoolController {
+
+	public static final String VIEW_PATH = "pwdtool/index";
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	/**
+	 * ツールの画面を表示する。
+	 */
+	@RequestMapping()
+	@Override
+	public ModelAndView index(Locale locale, SitePreference sitePreference,
+			HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView(VIEW_PATH);
+		return mav;
+	}
+
+	/**
+	 * 平文をエンコードした文字列を返却する。
+	 */
+	@RequestMapping(URI_PATH_ENCODE)
+	@ResponseBody
+	@Override
+	public String encode(@RequestParam(PARAM_PLAIN_TEXT) String plainText) {
+		return passwordEncoder.encode(plainText);
+	}
+
+}
