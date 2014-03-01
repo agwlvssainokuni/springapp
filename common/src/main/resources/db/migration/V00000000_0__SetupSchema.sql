5d5b9052041d3c33cf9c52bf6f640efcae2d2c4f
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-03-01 11:09:19 +0900

    common * 利用者管理。

--- common/src/main/resources/db/migration/V00000000_0__SetupSchema.sql
+++ common/src/main/resources/db/migration/V00000000_0__SetupSchema.sql
@@ -1,8 +1,53 @@
 -- Project Name : SpringApp
--- Date/Time    : 2014/03/01 10:28:58
+-- Date/Time    : 2014/03/01 11:02:14
 -- Author       : agwlvssainokuni
 -- RDBMS Type   : IBM DB2
 -- Application  : A5:SQL Mk-2
+
+-- メールテンプレート文面
+CREATE TABLE mail_template_texts( 
+	id INTEGER NOT NULL auto_increment, 
+	mail_template_id INTEGER NOT NULL, 
+	locale VARCHAR (5) NOT NULL, 
+	subject VARCHAR (1024) NOT NULL, 
+	body VARCHAR (4098) NOT NULL, 
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+	deleted_flg INTEGER DEFAULT 0 NOT NULL, 
+	CONSTRAINT mail_template_texts_pkc PRIMARY KEY (id)
+); 
+
+CREATE UNIQUE INDEX mail_template_texts_ix1 
+	ON mail_template_texts(mail_template_id, locale); 
+
+-- メールテンプレート宛先
+CREATE TABLE mail_template_addresses( 
+	id INTEGER NOT NULL auto_increment, 
+	mail_template_id INTEGER NOT NULL, 
+	mail_address VARCHAR (512) NOT NULL, 
+	type VARCHAR (3) NOT NULL CHECK type IN ('CC', 'BCC'), 
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+	deleted_flg INTEGER DEFAULT 0 NOT NULL, 
+	CONSTRAINT mail_template_addresses_pkc PRIMARY KEY (id)
+); 
+
+CREATE UNIQUE INDEX mail_template_addresses_ix1 
+	ON mail_template_addresses(mail_address, type); 
+
+CREATE INDEX mail_template_addresses_ix2 
+	ON mail_template_addresses(mail_template_id); 
+
+-- メールテンプレート
+CREATE TABLE mail_templates( 
+	id INTEGER NOT NULL auto_increment, 
+	name VARCHAR (32) NOT NULL, 
+	sender VARCHAR (512) NOT NULL, 
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+	deleted_flg INTEGER DEFAULT 0 NOT NULL, 
+	CONSTRAINT mail_templates_pkc PRIMARY KEY (id)
+); 
 
 -- 利用申請
 CREATE TABLE signup_entries( 
@@ -28,8 +73,8 @@ CREATE TABLE users(
 	mail_address VARCHAR (512) NOT NULL, 
 	password CHAR (60) NOT NULL, 
 	applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
-	first_name VARCHAR (32) NOT NULL, 
-	last_name VARCHAR (32) NOT NULL, 
+	first_name VARCHAR (64) NOT NULL, 
+	last_name VARCHAR (64) NOT NULL, 
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	deleted_flg INTEGER DEFAULT 0 NOT NULL, 
@@ -38,6 +83,78 @@ CREATE TABLE users(
 
 CREATE UNIQUE INDEX users_ix1 
 	ON users(mail_address); 
+
+COMMENT 
+	ON TABLE mail_template_texts IS 'メールテンプレート文面'; 
+
+COMMENT 
+	ON COLUMN mail_template_texts.id IS 'ID'; 
+
+COMMENT 
+	ON COLUMN mail_template_texts.mail_template_id IS 'メールテンプレートID'; 
+
+COMMENT 
+	ON COLUMN mail_template_texts.locale IS 'ロケール'; 
+
+COMMENT 
+	ON COLUMN mail_template_texts.subject IS '件名'; 
+
+COMMENT 
+	ON COLUMN mail_template_texts.body IS '本文'; 
+
+COMMENT 
+	ON COLUMN mail_template_texts.updated_at IS '更新日時'; 
+
+COMMENT 
+	ON COLUMN mail_template_texts.created_at IS '作成日時'; 
+
+COMMENT 
+	ON COLUMN mail_template_texts.deleted_flg IS '削除フラグ'; 
+
+COMMENT 
+	ON TABLE mail_template_addresses IS 'メールテンプレート宛先'; 
+
+COMMENT 
+	ON COLUMN mail_template_addresses.id IS 'ID'; 
+
+COMMENT 
+	ON COLUMN mail_template_addresses.mail_template_id IS 'メールテンプレートID'; 
+
+COMMENT 
+	ON COLUMN mail_template_addresses.mail_address IS 'メールアドレス'; 
+
+COMMENT 
+	ON COLUMN mail_template_addresses.type IS '宛先区分'; 
+
+COMMENT 
+	ON COLUMN mail_template_addresses.updated_at IS '更新日時'; 
+
+COMMENT 
+	ON COLUMN mail_template_addresses.created_at IS '作成日時'; 
+
+COMMENT 
+	ON COLUMN mail_template_addresses.deleted_flg IS '削除フラグ'; 
+
+COMMENT 
+	ON TABLE mail_templates IS 'メールテンプレート'; 
+
+COMMENT 
+	ON COLUMN mail_templates.id IS 'ID'; 
+
+COMMENT 
+	ON COLUMN mail_templates.name IS 'テンプレート名'; 
+
+COMMENT 
+	ON COLUMN mail_templates.sender IS '差出人'; 
+
+COMMENT 
+	ON COLUMN mail_templates.updated_at IS '更新日時'; 
+
+COMMENT 
+	ON COLUMN mail_templates.created_at IS '作成日時'; 
+
+COMMENT 
+	ON COLUMN mail_templates.deleted_flg IS '削除フラグ'; 
 
 COMMENT 
 	ON TABLE signup_entries IS '利用申請'; 
