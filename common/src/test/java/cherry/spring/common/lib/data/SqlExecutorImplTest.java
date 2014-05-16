84528166080d4437ef50d5ee5eef4f4edb3e8d8c
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-05-17 08:14:10 +0900

    common * ライセンス表記を修正。

--- common/src/test/java/cherry/spring/common/lib/data/SqlExecutorImplTest.java
+++ common/src/test/java/cherry/spring/common/lib/data/SqlExecutorImplTest.java
@@ -1,20 +1,20 @@
 /*
- *   Copyright 2012,2014 agwlvssainokuni
+ * Copyright 2012,2014 agwlvssainokuni
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package cherry.spring.common.lib.db;
+package cherry.spring.common.lib.data;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -29,9 +29,6 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.dao.DataAccessException;
-
-import cherry.spring.common.lib.data.SqlExecutor;
-import cherry.spring.common.lib.data.SqlExecutorImpl;
 
 /**
  * {@link SqlExecutorImpl} のテスト.
