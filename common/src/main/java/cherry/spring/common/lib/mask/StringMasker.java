bbf0b0725e6295351b717d3d7def2f45b3d5d844
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-09-20 01:12:22 +0900

    common * マスクユーティリティ。

---
+++ common/src/main/java/cherry/spring/common/lib/mask/StringMasker.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2014 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.spring.common.lib.mask;
+
+public abstract class StringMasker implements Masker<String> {
+
+	public static StringMasker nullMasker() {
+		return new NullMasker();
+	}
+
+	public static StringMasker tailMasker(int count, String maskChar) {
+		return new TailMasker(count, maskChar);
+	}
+
+	public static StringMasker fixedTailMasker(int count, String maskChar,
+			int fixedLength) {
+		return new FixedTailMasker(count, maskChar, fixedLength);
+	}
+
+	static class NullMasker extends StringMasker {
+
+		@Override
+		public String mask(String value) {
+			return value;
+		}
+	}
+
+	static class TailMasker extends StringMasker {
+
+		private final int count;
+
+		private final String maskChar;
+
+		public TailMasker(int count, String maskChar) {
+			this.count = count;
+			this.maskChar = maskChar;
+		}
+
+		@Override
+		public String mask(String value) {
+			StringBuilder s = new StringBuilder();
+			for (int i = 0; i < value.length(); i++) {
+				if (i < count) {
+					s.append(value.charAt(i));
+				} else {
+					s.append(maskChar);
+				}
+			}
+			return s.toString();
+		}
+	}
+
+	static class FixedTailMasker extends StringMasker {
+
+		private final int count;
+
+		private final String maskChar;
+
+		private final int fixedLength;
+
+		public FixedTailMasker(int count, String maskChar, int fixedLength) {
+			this.count = count;
+			this.maskChar = maskChar;
+			this.fixedLength = fixedLength;
+		}
+
+		@Override
+		public String mask(String value) {
+			StringBuilder s = new StringBuilder();
+			for (int i = 0; i < fixedLength; i++) {
+				if (i < count && i < value.length()) {
+					s.append(value.charAt(i));
+				} else {
+					s.append(maskChar);
+				}
+			}
+			return s.toString();
+		}
+	}
+
+}
