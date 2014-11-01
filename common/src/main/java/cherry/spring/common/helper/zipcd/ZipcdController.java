ae095febcd383ae319b906e91cfa4cf5af687ec1
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-11-01 17:48:38 +0900

    SpringApp * 郵便番号検索試行。

--- common/src/main/java/cherry/spring/common/helper/zipcd/ZipcdController.java
+++ common/src/main/java/cherry/spring/common/helper/zipcd/ZipcdController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import cherry.spring.common.api.ApiResponse;
 
-@RequestMapping("/secure/common/zipcd")
+@RequestMapping("${common.helper.zipcd.uripath}")
 public interface ZipcdController {
 
 	@RequestMapping()
