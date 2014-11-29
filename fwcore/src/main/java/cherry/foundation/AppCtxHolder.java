03514972f913c0ca495a9a766f92ae4b2b1d3801
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-11-30 07:29:37 +0900

    SpringApp * DI構成見直し。

--- fwcore/src/main/java/cherry/foundation/AppCtxHolder.java
+++ fwcore/src/main/java/cherry/foundation/AppCtxHolder.java
@@ -18,12 +18,10 @@ package cherry.foundation;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
 
 /**
  * アプリケーションコンテキスト ({@link ApplicationContext}) を保持する。
  */
-@Component
 public class AppCtxHolder implements ApplicationContextAware {
 
 	private static ApplicationContext appCtx;
