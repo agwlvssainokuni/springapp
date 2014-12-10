19329fd9ec85338f71c3a31ad8f11939d8dfc976
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-12-10 23:28:28 +0900

    SpringApp * 非同期実行フレームワーク。

--- dbschema/src/main/resources/db/migration/V00000000_1__Schema_AsyncProc.sql
+++ dbschema/src/main/resources/db/migration/V00000000_1__Schema_AsyncProc.sql
@@ -1,8 +1,118 @@
 -- Project Name : SpringApp
--- Date/Time    : 2014/08/24 20:14:14
+-- Date/Time    : 2014/12/10 23:27:02
 -- Author       : agwlvssainokuni
 -- RDBMS Type   : IBM DB2
 -- Application  : A5:SQL Mk-2
+
+-- 非同期実行状況管理・コマンド・結果
+CREATE TABLE async_process_command_result(
+	id INTEGER NOT NULL auto_increment,
+	async_process_id INTEGER NOT NULL,
+	exit_value INTEGER NOT NULL,
+	stdout VARCHAR (1024) NOT NULL,
+	stderr VARCHAR (1024) NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	lock_version INTEGER DEFAULT 1 NOT NULL,
+	deleted_flg INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT async_process_command_result_pkc PRIMARY KEY (id)
+);
+
+-- 非同期実行状況管理・コマンド・引数
+CREATE TABLE async_process_command_arg(
+	id INTEGER NOT NULL auto_increment,
+	async_process_id INTEGER NOT NULL,
+	argument VARCHAR (50) NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	lock_version INTEGER DEFAULT 1 NOT NULL,
+	deleted_flg INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT async_process_command_arg_pkc PRIMARY KEY (id)
+);
+
+-- 非同期実行状況管理・コマンド
+CREATE TABLE async_process_command(
+	id INTEGER NOT NULL auto_increment,
+	async_process_id INTEGER NOT NULL,
+	command VARCHAR (50) NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	lock_version INTEGER DEFAULT 1 NOT NULL,
+	deleted_flg INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT async_process_command_pkc PRIMARY KEY (id)
+);
+
+-- 非同期実行状況管理・ファイル処理・結果詳細
+CREATE TABLE async_process_file_result_detail(
+	id INTEGER NOT NULL auto_increment,
+	async_process_id INTEGER NOT NULL,
+	record_number bigint NOT NULL,
+	description VARCHAR (50),
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	lock_version INTEGER DEFAULT 1 NOT NULL,
+	deleted_flg INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT async_process_file_result_detail_pkc PRIMARY KEY (id)
+);
+
+-- 非同期実行状況管理・ファイル処理・結果
+CREATE TABLE async_process_file_result(
+	id INTEGER NOT NULL auto_increment,
+	async_process_id INTEGER NOT NULL,
+	total_count bigint NOT NULL,
+	ok_count bigint NOT NULL,
+	ng_count bigint NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	lock_version INTEGER DEFAULT 1 NOT NULL,
+	deleted_flg INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT async_process_file_result_pkc PRIMARY KEY (id)
+);
+
+-- 非同期実行状況管理・ファイル処理・引数
+CREATE TABLE async_process_file_arg(
+	id INTEGER NOT NULL auto_increment,
+	async_process_id INTEGER NOT NULL,
+	argument VARCHAR (50) NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	lock_version INTEGER DEFAULT 1 NOT NULL,
+	deleted_flg INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT async_process_file_arg_pkc PRIMARY KEY (id)
+);
+
+-- 非同期実行状況管理・ファイル処理
+CREATE TABLE async_process_file(
+	id INTEGER NOT NULL auto_increment,
+	async_process_id INTEGER NOT NULL,
+	handler_name VARCHAR (50) NOT NULL,
+	param_name VARCHAR (50) NOT NULL,
+	original_filename VARCHAR (50) NOT NULL,
+	content_type VARCHAR (50) NOT NULL,
+	file_size bigint NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	lock_version INTEGER DEFAULT 1 NOT NULL,
+	deleted_flg INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT async_process_file_pkc PRIMARY KEY (id)
+);
+
+-- 非同期実行状況管理
+CREATE TABLE async_process(
+	id INTEGER NOT NULL auto_increment,
+	launched_by VARCHAR (32) NOT NULL,
+	type VARCHAR (3) NOT NULL,
+	status VARCHAR (3) NOT NULL,
+	launched_at TIMESTAMP NOT NULL,
+	invoked_at TIMESTAMP,
+	started_at TIMESTAMP,
+	finished_at TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	lock_version INTEGER DEFAULT 1 NOT NULL,
+	deleted_flg INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT async_process_pkc PRIMARY KEY (id)
+);
 
 -- 非同期処理
 CREATE TABLE async_proc(
@@ -30,6 +140,240 @@ CREATE TABLE async_proc(
 
 CREATE INDEX async_proc_ix1
 	ON async_proc(launcher_id);
+
+COMMENT
+	ON TABLE async_process_command_result IS '非同期実行状況管理・コマンド・結果';
+
+COMMENT
+	ON COLUMN async_process_command_result.id IS 'ID';
+
+COMMENT
+	ON COLUMN async_process_command_result.async_process_id IS '非同期実行状況管理ID';
+
+COMMENT
+	ON COLUMN async_process_command_result.exit_value IS '終了コード';
+
+COMMENT
+	ON COLUMN async_process_command_result.stdout IS '標準出力';
+
+COMMENT
+	ON COLUMN async_process_command_result.stderr IS '標準エラー出力';
+
+COMMENT
+	ON COLUMN async_process_command_result.updated_at IS '更新日時';
+
+COMMENT
+	ON COLUMN async_process_command_result.created_at IS '作成日時';
+
+COMMENT
+	ON COLUMN async_process_command_result.lock_version IS 'ロックバージョン';
+
+COMMENT
+	ON COLUMN async_process_command_result.deleted_flg IS '削除フラグ';
+
+COMMENT
+	ON TABLE async_process_command_arg IS '非同期実行状況管理・コマンド・引数';
+
+COMMENT
+	ON COLUMN async_process_command_arg.id IS 'ID';
+
+COMMENT
+	ON COLUMN async_process_command_arg.async_process_id IS '非同期実行状況管理ID';
+
+COMMENT
+	ON COLUMN async_process_command_arg.argument IS '引数';
+
+COMMENT
+	ON COLUMN async_process_command_arg.updated_at IS '更新日時';
+
+COMMENT
+	ON COLUMN async_process_command_arg.created_at IS '作成日時';
+
+COMMENT
+	ON COLUMN async_process_command_arg.lock_version IS 'ロックバージョン';
+
+COMMENT
+	ON COLUMN async_process_command_arg.deleted_flg IS '削除フラグ';
+
+COMMENT
+	ON TABLE async_process_command IS '非同期実行状況管理・コマンド';
+
+COMMENT
+	ON COLUMN async_process_command.id IS 'ID';
+
+COMMENT
+	ON COLUMN async_process_command.async_process_id IS '非同期実行状況管理ID';
+
+COMMENT
+	ON COLUMN async_process_command.command IS 'コマンド';
+
+COMMENT
+	ON COLUMN async_process_command.updated_at IS '更新日時';
+
+COMMENT
+	ON COLUMN async_process_command.created_at IS '作成日時';
+
+COMMENT
+	ON COLUMN async_process_command.lock_version IS 'ロックバージョン';
+
+COMMENT
+	ON COLUMN async_process_command.deleted_flg IS '削除フラグ';
+
+COMMENT
+	ON TABLE async_process_file_result_detail IS '非同期実行状況管理・ファイル処理・結果詳細';
+
+COMMENT
+	ON COLUMN async_process_file_result_detail.id IS 'ID';
+
+COMMENT
+	ON COLUMN async_process_file_result_detail.async_process_id IS '非同期実行状況管理ID';
+
+COMMENT
+	ON COLUMN async_process_file_result_detail.record_number IS 'NGレコード番号';
+
+COMMENT
+	ON COLUMN async_process_file_result_detail.description IS '付加情報';
+
+COMMENT
+	ON COLUMN async_process_file_result_detail.updated_at IS '更新日時';
+
+COMMENT
+	ON COLUMN async_process_file_result_detail.created_at IS '作成日時';
+
+COMMENT
+	ON COLUMN async_process_file_result_detail.lock_version IS 'ロックバージョン';
+
+COMMENT
+	ON COLUMN async_process_file_result_detail.deleted_flg IS '削除フラグ';
+
+COMMENT
+	ON TABLE async_process_file_result IS '非同期実行状況管理・ファイル処理・結果';
+
+COMMENT
+	ON COLUMN async_process_file_result.id IS 'ID';
+
+COMMENT
+	ON COLUMN async_process_file_result.async_process_id IS '非同期実行状況管理ID';
+
+COMMENT
+	ON COLUMN async_process_file_result.total_count IS '総件数';
+
+COMMENT
+	ON COLUMN async_process_file_result.ok_count IS 'OK件数';
+
+COMMENT
+	ON COLUMN async_process_file_result.ng_count IS 'NG件数';
+
+COMMENT
+	ON COLUMN async_process_file_result.updated_at IS '更新日時';
+
+COMMENT
+	ON COLUMN async_process_file_result.created_at IS '作成日時';
+
+COMMENT
+	ON COLUMN async_process_file_result.lock_version IS 'ロックバージョン';
+
+COMMENT
+	ON COLUMN async_process_file_result.deleted_flg IS '削除フラグ';
+
+COMMENT
+	ON TABLE async_process_file_arg IS '非同期実行状況管理・ファイル処理・引数';
+
+COMMENT
+	ON COLUMN async_process_file_arg.id IS 'ID';
+
+COMMENT
+	ON COLUMN async_process_file_arg.async_process_id IS '非同期実行状況管理ID';
+
+COMMENT
+	ON COLUMN async_process_file_arg.argument IS '引数';
+
+COMMENT
+	ON COLUMN async_process_file_arg.updated_at IS '更新日時';
+
+COMMENT
+	ON COLUMN async_process_file_arg.created_at IS '作成日時';
+
+COMMENT
+	ON COLUMN async_process_file_arg.lock_version IS 'ロックバージョン';
+
+COMMENT
+	ON COLUMN async_process_file_arg.deleted_flg IS '削除フラグ';
+
+COMMENT
+	ON TABLE async_process_file IS '非同期実行状況管理・ファイル処理';
+
+COMMENT
+	ON COLUMN async_process_file.id IS 'ID';
+
+COMMENT
+	ON COLUMN async_process_file.async_process_id IS '非同期実行状況管理ID';
+
+COMMENT
+	ON COLUMN async_process_file.handler_name IS 'ハンドラ名';
+
+COMMENT
+	ON COLUMN async_process_file.param_name IS 'パラメタ名';
+
+COMMENT
+	ON COLUMN async_process_file.original_filename IS '元ファイル名';
+
+COMMENT
+	ON COLUMN async_process_file.content_type IS 'コンテントタイプ';
+
+COMMENT
+	ON COLUMN async_process_file.file_size IS 'ファイルサイズ';
+
+COMMENT
+	ON COLUMN async_process_file.updated_at IS '更新日時';
+
+COMMENT
+	ON COLUMN async_process_file.created_at IS '作成日時';
+
+COMMENT
+	ON COLUMN async_process_file.lock_version IS 'ロックバージョン';
+
+COMMENT
+	ON COLUMN async_process_file.deleted_flg IS '削除フラグ';
+
+COMMENT
+	ON TABLE async_process IS '非同期実行状況管理';
+
+COMMENT
+	ON COLUMN async_process.id IS 'ID';
+
+COMMENT
+	ON COLUMN async_process.launched_by IS 'キュー投入者ID';
+
+COMMENT
+	ON COLUMN async_process.type IS '非同期処理種別';
+
+COMMENT
+	ON COLUMN async_process.status IS 'ステータス';
+
+COMMENT
+	ON COLUMN async_process.launched_at IS '登録日時';
+
+COMMENT
+	ON COLUMN async_process.invoked_at IS 'キュー投入日時';
+
+COMMENT
+	ON COLUMN async_process.started_at IS '実行開始日時';
+
+COMMENT
+	ON COLUMN async_process.finished_at IS '実行終了日時';
+
+COMMENT
+	ON COLUMN async_process.updated_at IS '更新日時';
+
+COMMENT
+	ON COLUMN async_process.created_at IS '作成日時';
+
+COMMENT
+	ON COLUMN async_process.lock_version IS 'ロックバージョン';
+
+COMMENT
+	ON COLUMN async_process.deleted_flg IS '削除フラグ';
 
 COMMENT
 	ON TABLE async_proc IS '非同期処理';
