615a03321262a4815cc3138cb776a40c0c5d5c3c
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-12-20 23:31:12 +0900

    SpringApp * フォーム生成ツール。

---
+++ generator/generateform.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+set curdir=%~dp0
+
+:repeat
+
+echo %1
+set /a count=%count%+1
+set props=%props% -PdefinitionFile.%count%=%1
+
+if "%~2"=="" goto end
+shift
+goto repeat
+:end
+
+call gradle generateForm %props%
+
+pause
+
+@echo off
