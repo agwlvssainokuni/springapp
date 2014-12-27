ee2974b45df9f6e60570b291ade872970d9046c8
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-12-27 09:04:35 +0900

    SpringApp * クエリの書き方チュートリアル。

---
+++ querytutorial/src/test/java/cherry/querytutorial/querydsl/BasicUsageTest.java
@@ -0,0 +1,88 @@
+/*
+ *   Copyright 2014 agwlvssainokuni
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package cherry.querytutorial.querydsl;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jdbc.query.QueryDslJdbcOperations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cherry.querytutorial.db.gen.dto.Todo;
+import cherry.querytutorial.db.gen.query.QTodo;
+
+import com.mysema.query.Tuple;
+import com.mysema.query.sql.SQLQuery;
+import com.mysema.query.types.QBean;
+import com.mysema.query.types.QTuple;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:config/applicationContext.xml")
+public class BasicUsageTest {
+
+	@Autowired
+	private QueryDslJdbcOperations queryDslJdbcOperations;
+
+	@Test
+	public void test全列取得Tuple受け() {
+		SQLQuery query = queryDslJdbcOperations.newSqlQuery();
+		query.from(QTodo.todo);
+		List<Tuple> list = queryDslJdbcOperations.query(query, new QTuple(
+				QTodo.todo.all()));
+		assertThat(list, is(empty()));
+	}
+
+	@Test
+	public void test列指定Tuple受け() {
+		SQLQuery query = queryDslJdbcOperations.newSqlQuery();
+		query.from(QTodo.todo);
+		List<Tuple> list = queryDslJdbcOperations.query(query, new QTuple(
+				QTodo.todo.id, QTodo.todo.postedBy, QTodo.todo.postedAt,
+				QTodo.todo.dueDt, QTodo.todo.doneAt, QTodo.todo.doneFlg,
+				QTodo.todo.priorityCd, QTodo.todo.description));
+		assertThat(list, is(empty()));
+	}
+
+	@Test
+	public void test全列取得Bean受け() {
+		SQLQuery query = queryDslJdbcOperations.newSqlQuery();
+		query.from(QTodo.todo);
+		List<Todo> list = queryDslJdbcOperations.query(query, new QBean<Todo>(
+				Todo.class, QTodo.todo.all()));
+		assertThat(list, is(empty()));
+	}
+
+	@Test
+	public void test列指定Bean受け() {
+		SQLQuery query = queryDslJdbcOperations.newSqlQuery();
+		query.from(QTodo.todo);
+		List<Todo> list = queryDslJdbcOperations.query(query, new QBean<Todo>(
+				Todo.class, QTodo.todo.id, QTodo.todo.postedBy,
+				QTodo.todo.postedAt, QTodo.todo.dueDt, QTodo.todo.doneAt,
+				QTodo.todo.doneFlg, QTodo.todo.priorityCd,
+				QTodo.todo.description));
+		assertThat(list, is(empty()));
+	}
+
+}
