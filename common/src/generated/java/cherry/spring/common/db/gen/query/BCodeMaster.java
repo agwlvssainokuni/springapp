1f92c86a8e11c41cf3af925d645e591b3cd01506
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-01-17 19:57:12 +0900

    SpringApp * 区分値管理機能。

---
+++ common/src/generated/java/cherry/spring/common/db/gen/query/BCodeMaster.java
@@ -0,0 +1,106 @@
+package cherry.spring.common.db.gen.query;
+
+import javax.annotation.Generated;
+
+/**
+ * BCodeMaster is a Querydsl bean type
+ */
+@Generated("com.mysema.query.codegen.BeanSerializer")
+public class BCodeMaster {
+
+    private org.joda.time.LocalDateTime createdAt;
+
+    private Integer deletedFlg;
+
+    private Long id;
+
+    private String label;
+
+    private Integer lockVersion;
+
+    private String name;
+
+    private Integer sortOrder;
+
+    private org.joda.time.LocalDateTime updatedAt;
+
+    private String value;
+
+    public org.joda.time.LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(org.joda.time.LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getDeletedFlg() {
+        return deletedFlg;
+    }
+
+    public void setDeletedFlg(Integer deletedFlg) {
+        this.deletedFlg = deletedFlg;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Integer getLockVersion() {
+        return lockVersion;
+    }
+
+    public void setLockVersion(Integer lockVersion) {
+        this.lockVersion = lockVersion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public org.joda.time.LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(org.joda.time.LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+         return "createdAt = " + createdAt + ", deletedFlg = " + deletedFlg + ", id = " + id + ", label = " + label + ", lockVersion = " + lockVersion + ", name = " + name + ", sortOrder = " + sortOrder + ", updatedAt = " + updatedAt + ", value = " + value;
+    }
+
+}
+
