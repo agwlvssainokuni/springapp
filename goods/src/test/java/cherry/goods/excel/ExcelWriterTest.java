8af05d0168d85501a96bd84124514fb1983c8fe2
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-01-17 14:02:51 +0900

    SpringApp * Microsoft Excel入出力機能。

---
+++ goods/src/test/java/cherry/goods/excel/ExcelWriterTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2014,2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.goods.excel;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+
+public class ExcelWriterTest {
+
+	@Test
+	public void testGetNumberOfSheets() throws IOException {
+		try (Workbook workbook = new XSSFWorkbook()) {
+			// 準備
+			workbook.createSheet("CREATED 0");
+			workbook.createSheet("CREATED 1");
+			workbook.createSheet("CREATED 2");
+
+			// 実行＆検証
+			try (ExcelWriter writer = new ExcelWriter(workbook)) {
+				assertEquals(3, writer.getNumberOfSheets());
+			}
+		}
+	}
+
+	@Test
+	public void testGetSheetName() throws IOException {
+		try (Workbook workbook = new XSSFWorkbook()) {
+			// 準備
+			workbook.createSheet("CREATED 0");
+			workbook.createSheet("CREATED 1");
+			workbook.createSheet("CREATED 2");
+
+			// 実行＆検証
+			try (ExcelWriter writer = new ExcelWriter(workbook)) {
+				assertEquals("CREATED 0", writer.getSheetName(0));
+				assertEquals("CREATED 1", writer.getSheetName(1));
+				assertEquals("CREATED 2", writer.getSheetName(2));
+			}
+		}
+	}
+
+	@Test
+	public void testSetCurrentSheet() throws IOException {
+		try (Workbook workbook = new XSSFWorkbook()) {
+			// 準備
+			Sheet sheet0 = workbook.createSheet("CREATED 0");
+			Sheet sheet1 = workbook.createSheet("CREATED 1");
+
+			// 実行＆検証
+			try (ExcelWriter writer = new ExcelWriter(workbook)) {
+				writer.setCurrentSheet(0);
+				writer.write("CELL IN 0");
+				writer.setCurrentSheet(1);
+				writer.write("CELL IN 1");
+
+				assertEquals("CELL IN 0", sheet0.getRow(0).getCell(0).getStringCellValue());
+				assertEquals("CELL IN 1", sheet1.getRow(0).getCell(0).getStringCellValue());
+			}
+		}
+	}
+
+}
