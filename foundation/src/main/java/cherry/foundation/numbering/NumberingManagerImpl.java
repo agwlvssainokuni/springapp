86bd15e4aefdbc763ae280aa6af1fa91a0174b5f
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-02-01 01:18:39 +0900

    SpringApp * 発番機能。

--- foundation/src/main/java/cherry/foundation/numbering/NumberingManagerImpl.java
+++ foundation/src/main/java/cherry/foundation/numbering/NumberingManagerImpl.java
@@ -16,11 +16,55 @@
 
 package cherry.foundation.numbering;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+import java.text.MessageFormat;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import cherry.foundation.type.Code;
 
-public class NumberingManagerImpl implements NumberingManager {
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+public class NumberingManagerImpl implements NumberingManager, InitializingBean {
+
+	private NumberingStore numberingStore;
+
+	private long cacheSize;
+
+	private long cacheTimeoutSec;
+
+	private LoadingCache<String, NumberingDefinition> definitionCache;
+
+	public void setNumberingStore(NumberingStore numberingStore) {
+		this.numberingStore = numberingStore;
+	}
+
+	public void setCacheSize(long cacheSize) {
+		this.cacheSize = cacheSize;
+	}
+
+	public void setCacheTimeoutSec(long cacheTimeoutSec) {
+		this.cacheTimeoutSec = cacheTimeoutSec;
+	}
+
+	@Override
+	public void afterPropertiesSet() {
+		definitionCache = CacheBuilder.newBuilder().maximumSize(cacheSize)
+				.expireAfterAccess(cacheTimeoutSec, TimeUnit.SECONDS)
+				.build(new CacheLoader<String, NumberingDefinition>() {
+					@Override
+					public NumberingDefinition load(String key) {
+						return numberingStore.getDefinition(key);
+					}
+				});
+	}
 
 	@Transactional
 	@Override
@@ -37,15 +81,30 @@ public class NumberingManagerImpl implements NumberingManager {
 	@Transactional
 	@Override
 	public String issueAsString(String numberName) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		String[] values = issueAsString(numberName, 1);
+		return values[0];
 	}
 
 	@Transactional
 	@Override
 	public String[] issueAsString(String numberName, int count) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		checkArgument(count > 0, "count must be > 0");
+		try {
+			NumberingDefinition def = definitionCache.get(numberName);
+			MessageFormat fmt = new MessageFormat(def.getTemplate());
+			long current = numberingStore.loadCurrent(numberName);
+			String[] values = new String[count];
+			for (int i = 1; i <= count; i++) {
+				long val = current + i;
+				checkState(val >= def.getMinValue(), "{0} must be >= {1}", numberName, def.getMinValue());
+				checkState(val <= def.getMaxValue(), "{0} must be <= {1}", numberName, def.getMaxValue());
+				values[i] = fmt.format(val);
+			}
+			numberingStore.saveCurrent(numberName, current + count);
+			return values;
+		} catch (ExecutionException ex) {
+			throw new IllegalStateException(ex);
+		}
 	}
 
 	@Transactional
@@ -63,15 +122,29 @@ public class NumberingManagerImpl implements NumberingManager {
 	@Transactional
 	@Override
 	public long issueAsLong(String numberName) {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+		long[] values = issueAsLong(numberName, 1);
+		return values[0];
 	}
 
 	@Transactional
 	@Override
 	public long[] issueAsLong(String numberName, int count) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		checkArgument(count > 0, "count must be > 0");
+		try {
+			NumberingDefinition def = definitionCache.get(numberName);
+			long current = numberingStore.loadCurrent(numberName);
+			long[] values = new long[count];
+			for (int i = 1; i <= count; i++) {
+				long val = current + i;
+				checkState(val >= def.getMinValue(), "{0} must be >= {1}", numberName, def.getMinValue());
+				checkState(val <= def.getMaxValue(), "{0} must be <= {1}", numberName, def.getMaxValue());
+				values[i] = val;
+			}
+			numberingStore.saveCurrent(numberName, current + count);
+			return values;
+		} catch (ExecutionException ex) {
+			throw new IllegalStateException(ex);
+		}
 	}
 
 }
