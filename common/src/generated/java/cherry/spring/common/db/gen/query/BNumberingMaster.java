0cc567224e027d29ec84ac8a2ff2f0760dbace36
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-02-01 07:50:22 +0900

    SpringApp * 発番機能。

---
+++ common/src/generated/java/cherry/spring/common/db/gen/query/BNumberingMaster.java
@@ -0,0 +1,116 @@
+package cherry.spring.common.db.gen.query;
+
+import javax.annotation.Generated;
+
+/**
+ * BNumberingMaster is a Querydsl bean type
+ */
+@Generated("com.mysema.query.codegen.BeanSerializer")
+public class BNumberingMaster {
+
+    private org.joda.time.LocalDateTime createdAt;
+
+    private Long currentValue;
+
+    private Integer deletedFlg;
+
+    private Long id;
+
+    private Integer lockVersion;
+
+    private Long maxValue;
+
+    private Long minValue;
+
+    private String name;
+
+    private String template;
+
+    private org.joda.time.LocalDateTime updatedAt;
+
+    public org.joda.time.LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(org.joda.time.LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(Long currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public Integer getDeletedFlg() {
+        return deletedFlg;
+    }
+
+    public void setDeletedFlg(Integer deletedFlg) {
+        this.deletedFlg = deletedFlg;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getLockVersion() {
+        return lockVersion;
+    }
+
+    public void setLockVersion(Integer lockVersion) {
+        this.lockVersion = lockVersion;
+    }
+
+    public Long getMaxValue() {
+        return maxValue;
+    }
+
+    public void setMaxValue(Long maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    public Long getMinValue() {
+        return minValue;
+    }
+
+    public void setMinValue(Long minValue) {
+        this.minValue = minValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public org.joda.time.LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(org.joda.time.LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String toString() {
+         return "createdAt = " + createdAt + ", currentValue = " + currentValue + ", deletedFlg = " + deletedFlg + ", id = " + id + ", lockVersion = " + lockVersion + ", maxValue = " + maxValue + ", minValue = " + minValue + ", name = " + name + ", template = " + template + ", updatedAt = " + updatedAt;
+    }
+
+}
+
