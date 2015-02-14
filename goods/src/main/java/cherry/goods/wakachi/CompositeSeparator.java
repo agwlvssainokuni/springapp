246cb2d896b71d1c7c7ec99b5b9c2281e74d91b7
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-02-14 19:11:30 +0900

    SpringApp * 分かち書き機能。

---
+++ goods/src/main/java/cherry/goods/wakachi/CompositeSeparator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.goods.wakachi;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CompositeSeparator implements Separator {
+
+	private List<Separator> separators;
+
+	public void setSeparators(List<Separator> separators) {
+		this.separators = separators;
+	}
+
+	@Override
+	public List<String> separate(String text) {
+		List<String> list = Arrays.asList(text);
+		for (Separator sep : separators) {
+			list = doSeparate(sep, list);
+		}
+		return list;
+	}
+
+	private List<String> doSeparate(Separator sep, List<String> src) {
+		List<String> list = new LinkedList<>();
+		for (String text : src) {
+			list.addAll(sep.separate(text));
+		}
+		return list;
+	}
+
+}
---
+++ goods/src/main/java/cherry/goods/wakachi/SpaceCharSeparator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.goods.wakachi;
+
+import static java.lang.Character.isSpaceChar;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SpaceCharSeparator implements Separator {
+
+	@Override
+	public List<String> separate(String text) {
+		int beginIndex = -1;
+		List<String> list = new LinkedList<>();
+		for (int i = 0; i < text.length(); i++) {
+			if (isSpaceChar(text.charAt(i))) {
+				if (beginIndex >= 0) {
+					list.add(text.substring(beginIndex, i));
+					beginIndex = -1;
+				}
+			} else {
+				if (beginIndex < 0) {
+					beginIndex = i;
+				}
+			}
+		}
+		if (beginIndex >= 0) {
+			list.add(text.substring(beginIndex));
+		}
+		return list;
+	}
+
+}
