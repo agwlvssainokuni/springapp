7a23c61f45cc05c6f29569f8e0c1533e6693902d
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-02-14 19:54:56 +0900

    SpringApp * 分かち書き機能。

---
+++ goods/src/test/java/cherry/goods/wakachi/CompositeSeparatorTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.goods.wakachi;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CompositeSeparatorTest {
+
+	private CompositeSeparator separator;
+
+	@Before
+	public void before() {
+		SpaceCharSeparator sep0 = new SpaceCharSeparator();
+		UnicodeBlockSeparator sep1 = new UnicodeBlockSeparator();
+		NgramSeparator sep2 = new NgramSeparator();
+		sep2.setLength(2);
+		separator = new CompositeSeparator();
+		separator.setSeparators(asList(sep0, sep1, sep2));
+	}
+
+	@Test
+	public void testSeparate_EMPTY() {
+		List<String> list = separator.separate("");
+		assertEquals(0, list.size());
+	}
+
+	@Test
+	public void testSeparate_ASCII_SENTENCE() {
+		List<String> list = separator.separate("abc def ghi");
+		assertEquals(6, list.size());
+		assertEquals("ab", list.get(0));
+		assertEquals("bc", list.get(1));
+		assertEquals("de", list.get(2));
+		assertEquals("ef", list.get(3));
+		assertEquals("gh", list.get(4));
+		assertEquals("hi", list.get(5));
+	}
+
+	@Test
+	public void testSeparate_HIRA_KANJI_SENTENCE() {
+		List<String> list = separator.separate("これは、漢字文字列と、ひらがな文字列の、テストケース");
+		assertEquals(21, list.size());
+		assertEquals("これ", list.get(0));
+		assertEquals("れは", list.get(1));
+		assertEquals("、", list.get(2));
+		assertEquals("漢字", list.get(3));
+		assertEquals("字文", list.get(4));
+		assertEquals("文字", list.get(5));
+		assertEquals("字列", list.get(6));
+		assertEquals("と", list.get(7));
+		assertEquals("、", list.get(8));
+		assertEquals("ひら", list.get(9));
+		assertEquals("らが", list.get(10));
+		assertEquals("がな", list.get(11));
+		assertEquals("文字", list.get(12));
+		assertEquals("字列", list.get(13));
+		assertEquals("の", list.get(14));
+		assertEquals("、", list.get(15));
+		assertEquals("テス", list.get(16));
+		assertEquals("スト", list.get(17));
+		assertEquals("トケ", list.get(18));
+		assertEquals("ケー", list.get(19));
+		assertEquals("ース", list.get(20));
+	}
+
+}
