bb725acb16e2094b0132bd5c3c126191f7b281ee
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-02-24 06:11:15 +0900

    SpringApp * 営業日管理機能。

--- dbschema/src/main/resources/db/migration/V00000000_0__Schema_BizDateTime.sql
+++ dbschema/src/main/resources/db/migration/V00000000_0__Schema_BizDateTime.sql
@@ -1,8 +1,29 @@
 -- Project Name : SpringApp
--- Date/Time    : 2014/12/27 7:15:23
+-- Date/Time    : 2015/02/24 6:06:00
 -- Author       : agwlvssainokuni
 -- RDBMS Type   : IBM DB2
 -- Application  : A5:SQL Mk-2
+
+-- 桁マスタ
+create table digit (
+  d INTEGER not null
+  , updated_at TIMESTAMP default CURRENT_TIMESTAMP not null
+  , created_at TIMESTAMP default CURRENT_TIMESTAMP not null
+  , lock_version INTEGER default 1 not null
+  , deleted_flg INTEGER default 0 not null
+  , constraint digit_PKC primary key (d)
+) ;
+
+-- 休日マスタ
+create table holiday_master (
+  dt DATE not null
+  , type VARCHAR(2) default '00' not null
+  , updated_at TIMESTAMP default CURRENT_TIMESTAMP not null
+  , created_at TIMESTAMP default CURRENT_TIMESTAMP not null
+  , lock_version INTEGER default 1 not null
+  , deleted_flg INTEGER default 0 not null
+  , constraint holiday_master_PKC primary key (dt)
+) ;
 
 -- 業務日時マスタ
 create table bizdatetime_master (
@@ -18,6 +39,21 @@ create table bizdatetime_master (
   , deleted_flg INTEGER default 0 not null
   , constraint bizdatetime_master_PKC primary key (id)
 ) ;
+
+comment on table digit is '桁マスタ';
+comment on column digit.d is '桁';
+comment on column digit.updated_at is '更新日時';
+comment on column digit.created_at is '作成日時';
+comment on column digit.lock_version is 'ロックバージョン';
+comment on column digit.deleted_flg is '削除フラグ';
+
+comment on table holiday_master is '休日マスタ';
+comment on column holiday_master.dt is '年月日';
+comment on column holiday_master.type is '休日区分';
+comment on column holiday_master.updated_at is '更新日時';
+comment on column holiday_master.created_at is '作成日時';
+comment on column holiday_master.lock_version is 'ロックバージョン';
+comment on column holiday_master.deleted_flg is '削除フラグ';
 
 comment on table bizdatetime_master is '業務日時マスタ';
 comment on column bizdatetime_master.id is 'ID';
