9dcc0189eec28a68b9589e5e643587cc1391ebe6
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-02-25 08:41:21 +0900

    SpringApp * 営業日管理機能。

--- common/src/test/java/cherry/spring/common/foundation/impl/WorkdayStoreImplTest.java
+++ common/src/test/java/cherry/spring/common/foundation/impl/WorkdayStoreImplTest.java
@@ -16,7 +16,11 @@
 
 package cherry.spring.common.foundation.impl;
 
-import org.joda.time.LocalDate;
+import static org.joda.time.LocalDate.now;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +28,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cherry.foundation.workday.WorkdayStore;
-
-import com.ibm.icu.text.MessageFormat;
+import cherry.spring.common.db.gen.dto.DayoffMaster;
+import cherry.spring.common.db.gen.dto.DayoffMasterCriteria;
+import cherry.spring.common.db.gen.mapper.DayoffMasterMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:config/applicationContext-test.xml")
@@ -34,21 +39,85 @@ public class WorkdayStoreImplTest {
 	@Autowired
 	private WorkdayStore workdayStore;
 
+	@Autowired
+	private DayoffMasterMapper mapper;
+
+	@Before
+	public void before() {
+		createDayoffMaster("TEST", 3);
+		createDayoffMaster("TEST", 4);
+		createDayoffMaster("TEST", 8);
+		createDayoffMaster("TEST", 9);
+	}
+
+	@After
+	public void after() {
+		DayoffMasterCriteria c = new DayoffMasterCriteria();
+		c.createCriteria().andNameEqualTo("TEST");
+		mapper.deleteByExample(c);
+	}
+
 	@Test
 	public void testGetNumberOfWorkday() {
-		for (int i = 0; i < 50; i++) {
-			LocalDate dt = LocalDate.now().plusDays(i);
-			int numOfDays = workdayStore.getNumberOfWorkday("standard", LocalDate.now(), dt);
-			System.out.println(MessageFormat.format("{0,date,yyyy/MM/dd} {1,number,0000}", dt.toDate(), numOfDays));
-		}
+		// 基本形
+		assertEquals(1, workdayStore.getNumberOfWorkday("TEST", now(), now()));
+		assertEquals(2, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(1)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(2)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(3)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(4)));
+		assertEquals(4, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(5)));
+		assertEquals(5, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(6)));
+		assertEquals(6, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(7)));
+		assertEquals(6, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(8)));
+		assertEquals(6, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(9)));
+		assertEquals(7, workdayStore.getNumberOfWorkday("TEST", now(), now().plusDays(10)));
+		// 起点日が非営業日(1日目)
+		assertEquals(0, workdayStore.getNumberOfWorkday("TEST", now().plusDays(3), now().plusDays(3)));
+		assertEquals(0, workdayStore.getNumberOfWorkday("TEST", now().plusDays(3), now().plusDays(4)));
+		assertEquals(1, workdayStore.getNumberOfWorkday("TEST", now().plusDays(3), now().plusDays(5)));
+		assertEquals(2, workdayStore.getNumberOfWorkday("TEST", now().plusDays(3), now().plusDays(6)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now().plusDays(3), now().plusDays(7)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now().plusDays(3), now().plusDays(8)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now().plusDays(3), now().plusDays(9)));
+		assertEquals(4, workdayStore.getNumberOfWorkday("TEST", now().plusDays(3), now().plusDays(10)));
+		// 起点日が非営業日(2日目)
+		assertEquals(0, workdayStore.getNumberOfWorkday("TEST", now().plusDays(4), now().plusDays(4)));
+		assertEquals(1, workdayStore.getNumberOfWorkday("TEST", now().plusDays(4), now().plusDays(5)));
+		assertEquals(2, workdayStore.getNumberOfWorkday("TEST", now().plusDays(4), now().plusDays(6)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now().plusDays(4), now().plusDays(7)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now().plusDays(4), now().plusDays(8)));
+		assertEquals(3, workdayStore.getNumberOfWorkday("TEST", now().plusDays(4), now().plusDays(9)));
+		assertEquals(4, workdayStore.getNumberOfWorkday("TEST", now().plusDays(4), now().plusDays(10)));
 	}
 
 	@Test
 	public void testGetNextWorkday() {
-		for (int i = 1; i <= 50; i++) {
-			LocalDate dt = workdayStore.getNextWorkday("standard", LocalDate.now(), i);
-			System.out.println(MessageFormat.format("{0,number,0000} {1,date,yyyy/MM/dd}", i, dt.toDate()));
-		}
+		// 基本形
+		assertEquals(now(), workdayStore.getNextWorkday("TEST", now(), 1));
+		assertEquals(now().plusDays(1), workdayStore.getNextWorkday("TEST", now(), 2));
+		assertEquals(now().plusDays(2), workdayStore.getNextWorkday("TEST", now(), 3));
+		assertEquals(now().plusDays(5), workdayStore.getNextWorkday("TEST", now(), 4));
+		assertEquals(now().plusDays(6), workdayStore.getNextWorkday("TEST", now(), 5));
+		assertEquals(now().plusDays(7), workdayStore.getNextWorkday("TEST", now(), 6));
+		assertEquals(now().plusDays(10), workdayStore.getNextWorkday("TEST", now(), 7));
+		// 起点日が非営業日(1日目)
+		assertEquals(now().plusDays(5), workdayStore.getNextWorkday("TEST", now().plusDays(3), 1));
+		assertEquals(now().plusDays(6), workdayStore.getNextWorkday("TEST", now().plusDays(3), 2));
+		assertEquals(now().plusDays(7), workdayStore.getNextWorkday("TEST", now().plusDays(3), 3));
+		assertEquals(now().plusDays(10), workdayStore.getNextWorkday("TEST", now().plusDays(3), 4));
+		// 起点日が非営業日(1日目)
+		assertEquals(now().plusDays(5), workdayStore.getNextWorkday("TEST", now().plusDays(4), 1));
+		assertEquals(now().plusDays(6), workdayStore.getNextWorkday("TEST", now().plusDays(4), 2));
+		assertEquals(now().plusDays(7), workdayStore.getNextWorkday("TEST", now().plusDays(4), 3));
+		assertEquals(now().plusDays(10), workdayStore.getNextWorkday("TEST", now().plusDays(4), 4));
+	}
+
+	private void createDayoffMaster(String name, int n) {
+		DayoffMaster record;
+		record = new DayoffMaster();
+		record.setName(name);
+		record.setDt(now().plusDays(n));
+		mapper.insertSelective(record);
 	}
 
 }
