e129d7c0ae82b1f27e0e12222e475dc11b6580af
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-02-25 22:11:10 +0900

    SpringApp * 営業日管理機能。

--- common/src/main/java/cherry/spring/common/foundation/impl/WorkdayStoreImpl.java
+++ common/src/main/java/cherry/spring/common/foundation/impl/WorkdayStoreImpl.java
@@ -55,13 +55,6 @@ public class WorkdayStoreImpl implements WorkdayStore {
 
 	@Override
 	public int getNumberOfWorkday(String name, LocalDate from, LocalDate to) {
-		// SELECT COUNT(H0.dt) FROM dayoff_master H0
-		// WHERE
-		// H0.name = :name
-		// AND
-		// H0.dt BETWEEN :from AND :to
-		// AND
-		// H0.deleted_flg = 0
 		SQLQuery query = queryDslJdbcOperations.newSqlQuery();
 		query.from(h0);
 		query.where(h0.name.eq(name), h0.dt.between(constant(from), constant(to)), h0.deletedFlg.eq(NOT_DELETED.code()));
@@ -71,24 +64,6 @@ public class WorkdayStoreImpl implements WorkdayStore {
 
 	@Override
 	public LocalDate getNextWorkday(String name, LocalDate from, int numberOfWorkday) {
-
-		// SELECT
-		// MIN(D.dt)
-		// FROM
-		// (SELECT A.d*10+B.d AS n, ADD_DAYS(:from, A.d*10+B.d) AS dt FROM digit A, digit B) D
-		// LEFT OUTER JOIN dayoff_master H0
-		// ON
-		// H0.name = :name
-		// H0.dt BETWEEN :from AND D.dt
-		// AND
-		// H0.deleted_flg = 0
-		// WHERE
-		// NOT EXISTS (SELECT 1 FROM dayoff_master H1 WHERE H1.name = :name AND H1.dt = D.dt AND H1.deleted_flg = 0)
-		// GROUP BY
-		// D.n
-		// HAVING
-		// COUNT(H0.dt) = D.n - :n + 1
-
 		ListSubQuery<Tuple> subquery = new SQLSubQuery().from(a, b).list(
 				a.d.multiply(10).add(b.d).as("n"),
 				dateOperation(LocalDate.class, DateTimeOps.ADD_DAYS, constant(from), a.d.multiply(10).add(b.d))
