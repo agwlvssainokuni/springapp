e5278f7c94638e4055c63b87b7d1e491f9430b74
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-02-26 22:55:25 +0900

    SpringApp * 数値操作ユーティリティ。

--- goods/src/main/java/cherry/goods/util/NumberUtil.java
+++ goods/src/main/java/cherry/goods/util/NumberUtil.java
@@ -24,102 +24,122 @@ import java.math.RoundingMode;
  */
 public class NumberUtil {
 
-	/** 小数点第1位で四捨五入。 */
+	/**
+	 * 数値をスケール指定して丸める。<br />
+	 * 小数値 ({@link BigDecimal}, {@link Double}, {@link Float}) を対象とする。整数値は丸めない (そのまま返却する)。
+	 * 
+	 * @param number 丸める対象の数値。
+	 * @param newScale スケール。
+	 * @param roundingMode 丸め方。
+	 * @return 丸めた数値。
+	 */
+	public static Number setScale(Number number, int newScale, RoundingMode roundingMode) {
+		if (number instanceof BigDecimal) {
+			return ((BigDecimal) number).setScale(newScale, roundingMode);
+		} else if (number instanceof Double || number instanceof Float) {
+			BigDecimal d = BigDecimal.valueOf(number.doubleValue());
+			return Double.valueOf(d.setScale(newScale, roundingMode).doubleValue());
+		} else {
+			return number;
+		}
+	}
+
+	/** 小数第1位で四捨五入。 */
 	public static BigDecimal halfUp1(BigDecimal number) {
 		return number.setScale(0, RoundingMode.HALF_UP);
 	}
 
-	/** 小数点第2位で四捨五入。 */
+	/** 小数第2位で四捨五入。 */
 	public static BigDecimal halfUp2(BigDecimal number) {
 		return number.setScale(1, RoundingMode.HALF_UP);
 	}
 
-	/** 小数点第3位で四捨五入。 */
+	/** 小数第3位で四捨五入。 */
 	public static BigDecimal halfUp3(BigDecimal number) {
 		return number.setScale(2, RoundingMode.HALF_UP);
 	}
 
-	/** 小数点第4位で四捨五入。 */
+	/** 小数第4位で四捨五入。 */
 	public static BigDecimal halfUp4(BigDecimal number) {
 		return number.setScale(3, RoundingMode.HALF_UP);
 	}
 
-	/** 小数点第1位で切上げ (絶対値が大きくなる方向へ切上げ)。 */
+	/** 小数第1位で切上げ (絶対値が大きくなる方向へ切上げ)。 */
 	public static BigDecimal roundUp1(BigDecimal number) {
 		return number.setScale(0, RoundingMode.UP);
 	}
 
-	/** 小数点第2位で切上げ (絶対値が大きくなる方向へ切上げ)。 */
+	/** 小数第2位で切上げ (絶対値が大きくなる方向へ切上げ)。 */
 	public static BigDecimal roundUp2(BigDecimal number) {
 		return number.setScale(1, RoundingMode.UP);
 	}
 
-	/** 小数点第3位で切上げ (絶対値が大きくなる方向へ切上げ)。 */
+	/** 小数第3位で切上げ (絶対値が大きくなる方向へ切上げ)。 */
 	public static BigDecimal roundUp3(BigDecimal number) {
 		return number.setScale(2, RoundingMode.UP);
 	}
 
-	/** 小数点第4位で切上げ (絶対値が大きくなる方向へ切上げ)。 */
+	/** 小数第4位で切上げ (絶対値が大きくなる方向へ切上げ)。 */
 	public static BigDecimal roundUp4(BigDecimal number) {
 		return number.setScale(3, RoundingMode.UP);
 	}
 
-	/** 小数点第1位で切下げ (絶対値が小さくなる方向へ切下げ)。 */
+	/** 小数第1位で切下げ (絶対値が小さくなる方向へ切下げ)。 */
 	public static BigDecimal roundDown1(BigDecimal number) {
 		return number.setScale(0, RoundingMode.DOWN);
 	}
 
-	/** 小数点第2位で切下げ (絶対値が小さくなる方向へ切下げ)。 */
+	/** 小数第2位で切下げ (絶対値が小さくなる方向へ切下げ)。 */
 	public static BigDecimal roundDown2(BigDecimal number) {
 		return number.setScale(1, RoundingMode.DOWN);
 	}
 
-	/** 小数点第3位で切下げ (絶対値が小さくなる方向へ切下げ)。 */
+	/** 小数第3位で切下げ (絶対値が小さくなる方向へ切下げ)。 */
 	public static BigDecimal roundDown3(BigDecimal number) {
 		return number.setScale(2, RoundingMode.DOWN);
 	}
 
-	/** 小数点第4位で切下げ (絶対値が小さくなる方向へ切下げ)。 */
+	/** 小数第4位で切下げ (絶対値が小さくなる方向へ切下げ)。 */
 	public static BigDecimal roundDown4(BigDecimal number) {
 		return number.setScale(3, RoundingMode.DOWN);
 	}
 
-	/** 小数点第1位で切上げ (大きくなる方向へ切上げ)。 */
+	/** 小数第1位で切上げ (大きくなる方向へ切上げ)。 */
 	public static BigDecimal ceiling1(BigDecimal number) {
 		return number.setScale(0, RoundingMode.CEILING);
 	}
 
-	/** 小数点第2位で切上げ (大きくなる方向へ切上げ)。 */
+	/** 小数第2位で切上げ (大きくなる方向へ切上げ)。 */
 	public static BigDecimal ceiling2(BigDecimal number) {
 		return number.setScale(1, RoundingMode.CEILING);
 	}
 
-	/** 小数点第3位で切上げ (大きくなる方向へ切上げ)。 */
+	/** 小数第3位で切上げ (大きくなる方向へ切上げ)。 */
 	public static BigDecimal ceiling3(BigDecimal number) {
 		return number.setScale(2, RoundingMode.CEILING);
 	}
 
-	/** 小数点第4位で切上げ (大きくなる方向へ切上げ)。 */
+	/** 小数第4位で切上げ (大きくなる方向へ切上げ)。 */
 	public static BigDecimal ceiling4(BigDecimal number) {
 		return number.setScale(3, RoundingMode.CEILING);
 	}
 
-	/** 小数点第1位で切下げ (小さくなる方向へ切下げ)。 */
+	/** 小数第1位で切下げ (小さくなる方向へ切下げ)。 */
 	public static BigDecimal floor1(BigDecimal number) {
 		return number.setScale(0, RoundingMode.FLOOR);
 	}
 
-	/** 小数点第2位で切下げ (小さくなる方向へ切下げ)。 */
+	/** 小数第2位で切下げ (小さくなる方向へ切下げ)。 */
 	public static BigDecimal floor2(BigDecimal number) {
 		return number.setScale(1, RoundingMode.FLOOR);
 	}
 
-	/** 小数点第3位で切下げ (小さくなる方向へ切下げ)。 */
+	/** 小数第3位で切下げ (小さくなる方向へ切下げ)。 */
 	public static BigDecimal floor3(BigDecimal number) {
 		return number.setScale(2, RoundingMode.FLOOR);
 	}
 
-	/** 小数点第4位で切下げ (小さくなる方向へ切下げ)。 */
+	/** 小数第4位で切下げ (小さくなる方向へ切下げ)。 */
 	public static BigDecimal floor4(BigDecimal number) {
 		return number.setScale(3, RoundingMode.FLOOR);
 	}
