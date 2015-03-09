a6c48c153de3e9faf6e1c0dc6903a29235b440f2
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-03-09 20:58:11 +0900

    SpringApp * 全角ひらがな、全角カタカナの文字種判定について、カギ括弧「」、二重カギ括弧『』、句読点「、。」をOKとする。

--- foundation/src/main/java/cherry/foundation/validator/CharTypeFullKatakana.java
+++ foundation/src/main/java/cherry/foundation/validator/CharTypeFullKatakana.java
@@ -35,7 +35,7 @@ import cherry.foundation.validator.CharType.Mode;
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
 @Retention(RUNTIME)
 @Constraint(validatedBy = {})
-@CharType(Mode.FullKatakana)
+@CharType(value = Mode.FullKatakana, acceptable = "「」『』、。")
 @ReportAsSingleViolation
 public @interface CharTypeFullKatakana {
 
