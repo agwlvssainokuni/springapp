7d81271907c64bcacfa85c31da8ba3a7e8c9bcf0
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-06-24 20:46:28 +0900

    foundation: 単項目バリデーションヘルパ
    Spring MVCの枠組みの外でアノテーションによる単項目チェックする機能を提供する。

--- foundation/src/main/java/cherry/foundation/validator/DataBinderHelper.java
+++ foundation/src/main/java/cherry/foundation/validator/DataBinderHelper.java
@@ -22,18 +22,66 @@ import java.util.Locale;
 import org.springframework.beans.PropertyValues;
 import org.springframework.validation.BindingResult;
 
+/**
+ * 単項目バリデーション適用機能。<br />
+ * Spring MVCの枠組みの外でアノテーション指定に基づき単項目チェックする機能を提供する。
+ */
 public interface DataBinderHelper {
 
+	/**
+	 * 対象オブジェクトに値を格納し (bind)、該当プロパティに指定されたアノテーションに基づき単項目チェックする (validate)。
+	 * 
+	 * @param target 対象オブジェクト。
+	 * @param pvs 対象オブジェクトに格納する値。
+	 * @return 単項目チェックの結果。
+	 */
 	BindingResult bindAndValidate(Object target, PropertyValues pvs);
 
+	/**
+	 * 対象オブジェクトに値を格納し (bind)、該当プロパティに指定されたアノテーションに基づき単項目チェックする (validate)。
+	 * 
+	 * @param target 対象オブジェクト。
+	 * @param objectName 対象オブジェクトの名称 (物理名称 = 項目名を名前解決する際のForm名)。
+	 * @param pvs 対象オブジェクトに格納する値。
+	 * @return 単項目チェックの結果。
+	 */
 	BindingResult bindAndValidate(Object target, String objectName, PropertyValues pvs);
 
+	/**
+	 * 単項目チェックの結果を全てエラーメッセージ (文字列) として取り出す。
+	 * 
+	 * @param binding 単項目チェックの結果。
+	 * @param locale 取り出すメッセージ (文字列) の言語指定。
+	 * @return エラーメッセージ (文字列) のリスト。
+	 */
 	List<String> resolveAllMessage(BindingResult binding, Locale locale);
 
+	/**
+	 * 単項目チェックの結果のうちフィールドに紐付かないものをエラーメッセージ (文字列) として取り出す。
+	 * 
+	 * @param binding 単項目チェックの結果。
+	 * @param locale 取り出すメッセージ (文字列) の言語指定。
+	 * @return エラーメッセージ (文字列) のリスト。
+	 */
 	List<String> resolveGlobalMessage(BindingResult binding, Locale locale);
 
+	/**
+	 * 単項目チェックの結果のうちフィールドに紐付くものをエラーメッセージ (文字列) として取り出す。
+	 * 
+	 * @param binding 単項目チェックの結果。
+	 * @param locale 取り出すメッセージ (文字列) の言語指定。
+	 * @return エラーメッセージ (文字列) のリスト。
+	 */
 	List<String> resolveFieldMessage(BindingResult binding, Locale locale);
 
+	/**
+	 * 単項目の結果のうち指定されたフィールドに紐付くものをエラーメッセージ (文字列) として取り出す。
+	 * 
+	 * @param binding 単項目チェックの結果。
+	 * @param field フィールド名称 (物理名称)。
+	 * @param locale 取り出すメッセージ (文字列) の言語指定。
+	 * @return エラーメッセージ (文字列) のリスト。
+	 */
 	List<String> resolveFieldMessage(BindingResult binding, String field, Locale locale);
 
 }
