1219c457f772c7e17fce1f8065db37bf613e2b69
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-06-28 22:44:40 +0900

    common: メール送信機能
    DB永続化処理を見直し。

--- common/src/main/java/cherry/spring/common/foundation/impl/TemplateStoreImpl.java
+++ common/src/main/java/cherry/spring/common/foundation/impl/TemplateStoreImpl.java
@@ -18,16 +18,10 @@ package cherry.spring.common.foundation.impl;
 
 import static com.google.common.base.Preconditions.checkState;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jdbc.query.QueryDslJdbcOperations;
-import org.springframework.data.jdbc.query.SqlDeleteCallback;
-import org.springframework.data.jdbc.query.SqlInsertCallback;
-import org.springframework.data.jdbc.query.SqlInsertWithKeyCallback;
-import org.springframework.data.jdbc.query.SqlUpdateCallback;
 import org.springframework.transaction.annotation.Transactional;
 
 import cherry.foundation.mail.MailData;
@@ -38,15 +32,14 @@ import cherry.spring.common.db.gen.query.QMailTemplateRcpt;
 
 import com.mysema.query.Tuple;
 import com.mysema.query.sql.SQLQuery;
-import com.mysema.query.sql.dml.SQLDeleteClause;
+import com.mysema.query.sql.SQLQueryFactory;
 import com.mysema.query.sql.dml.SQLInsertClause;
 import com.mysema.query.sql.dml.SQLUpdateClause;
-import com.mysema.query.types.QTuple;
 
 public class TemplateStoreImpl implements TemplateStore {
 
 	@Autowired
-	private QueryDslJdbcOperations queryDslJdbcOperations;
+	private SQLQueryFactory queryFactory;
 
 	private final QMailTemplate mt = new QMailTemplate("mt");
 	private final QMailTemplateRcpt mtr = new QMailTemplateRcpt("mtr");
@@ -55,16 +48,13 @@ public class TemplateStoreImpl implements TemplateStore {
 	@Override
 	public MailData getTemplate(String templateName) {
 
-		SQLQuery querya = queryDslJdbcOperations.newSqlQuery();
-		querya.from(mt);
+		SQLQuery querya = queryFactory.from(mt);
 		querya.where(mt.templateName.eq(templateName), mt.deletedFlg.eq(DeletedFlag.NOT_DELETED.code()));
-		Tuple templ = queryDslJdbcOperations.queryForObject(querya, new QTuple(mt.id, mt.fromAddr, mt.replyToAddr,
-				mt.subject, mt.body));
+		Tuple templ = querya.uniqueResult(mt.id, mt.fromAddr, mt.replyToAddr, mt.subject, mt.body);
 
-		SQLQuery queryb = queryDslJdbcOperations.newSqlQuery();
-		queryb.from(mtr);
+		SQLQuery queryb = queryFactory.from(mtr);
 		queryb.where(mtr.templateId.eq(templ.get(mt.id)), mtr.deletedFlg.eq(DeletedFlag.NOT_DELETED.code()));
-		List<Tuple> templAddr = queryDslJdbcOperations.query(queryb, new QTuple(mtr.rcptType, mtr.rcptAddr));
+		List<Tuple> templAddr = queryb.list(mtr.rcptType, mtr.rcptAddr);
 
 		List<String> toAddr = new ArrayList<>();
 		List<String> ccAddr = new ArrayList<>();
@@ -95,60 +85,39 @@ public class TemplateStoreImpl implements TemplateStore {
 
 	@Transactional
 	@Override
-	public void putTemplate(final String templateName, final MailData mailData) {
+	public void putTemplate(String templateName, MailData mailData) {
 
-		SQLQuery querya = queryDslJdbcOperations.newSqlQuery();
-		querya.from(mt);
+		SQLQuery querya = queryFactory.from(mt);
 		querya.where(mt.templateName.eq(templateName), mt.deletedFlg.eq(DeletedFlag.NOT_DELETED.code()));
 		querya.forUpdate();
-		Long id = queryDslJdbcOperations.queryForObject(querya, mt.id);
+		Long id = querya.uniqueResult(mt.id);
 		if (id == null) {
-			id = queryDslJdbcOperations.insertWithKey(mt, new SqlInsertWithKeyCallback<Long>() {
-				@Override
-				public Long doInSqlInsertWithKeyClause(SQLInsertClause insert) throws SQLException {
-					insert.set(mt.templateName, templateName);
-					insert.set(mt.fromAddr, mailData.getFromAddr());
-					insert.set(mt.replyToAddr, mailData.getReplyToAddr());
-					insert.set(mt.subject, mailData.getSubject());
-					insert.set(mt.body, mailData.getBody());
-					return insert.executeWithKey(Long.class);
-				}
-			});
+			SQLInsertClause insert = queryFactory.insert(mt);
+			insert.set(mt.templateName, templateName);
+			insert.set(mt.fromAddr, mailData.getFromAddr());
+			insert.set(mt.replyToAddr, mailData.getReplyToAddr());
+			insert.set(mt.subject, mailData.getSubject());
+			insert.set(mt.body, mailData.getBody());
+			id = insert.executeWithKey(Long.class);
 			checkState(id != 0, "Failed to create %s: %s", mt.getTableName(), mailData.toString());
 		} else {
-			final Long fId = id;
-			long count = queryDslJdbcOperations.update(mt, new SqlUpdateCallback() {
-				@Override
-				public long doInSqlUpdateClause(SQLUpdateClause update) {
-					update.set(mt.fromAddr, mailData.getFromAddr());
-					update.set(mt.replyToAddr, mailData.getReplyToAddr());
-					update.set(mt.subject, mailData.getSubject());
-					update.set(mt.body, mailData.getBody());
-					update.set(mt.lockVersion, mt.lockVersion.add(1));
-					update.where(mt.id.eq(fId));
-					return update.execute();
-				}
-			});
+			SQLUpdateClause update = queryFactory.update(mt);
+			update.set(mt.fromAddr, mailData.getFromAddr());
+			update.set(mt.replyToAddr, mailData.getReplyToAddr());
+			update.set(mt.subject, mailData.getSubject());
+			update.set(mt.body, mailData.getBody());
+			update.set(mt.lockVersion, mt.lockVersion.add(1));
+			update.where(mt.id.eq(id));
+			long count = update.execute();
 			checkState(count == 1, "Failed to update %s: %s", mt.getTableName(), mailData.toString());
 		}
 
-		final Long templateId = id;
-		queryDslJdbcOperations.delete(mtr, new SqlDeleteCallback() {
-			@Override
-			public long doInSqlDeleteClause(SQLDeleteClause delete) {
-				delete.where(mtr.templateId.eq(templateId));
-				return delete.execute();
-			}
-		});
-		long count = queryDslJdbcOperations.insert(mtr, new SqlInsertCallback() {
-			@Override
-			public long doInSqlInsertClause(SQLInsertClause insert) {
-				setupInsertClause(insert, mtr, templateId, RcptType.TO.name(), mailData.getToAddr());
-				setupInsertClause(insert, mtr, templateId, RcptType.CC.name(), mailData.getCcAddr());
-				setupInsertClause(insert, mtr, templateId, RcptType.BCC.name(), mailData.getBccAddr());
-				return insert.execute();
-			}
-		});
+		queryFactory.delete(mtr).where(mtr.templateId.eq(id)).execute();
+		SQLInsertClause insert = queryFactory.insert(mtr);
+		setupInsertClause(insert, mtr, id, RcptType.TO.name(), mailData.getToAddr());
+		setupInsertClause(insert, mtr, id, RcptType.CC.name(), mailData.getCcAddr());
+		setupInsertClause(insert, mtr, id, RcptType.BCC.name(), mailData.getBccAddr());
+		long count = insert.execute();
 		long numOfAddr = (mailData.getToAddr() == null ? 0L : mailData.getToAddr().size())
 				+ (mailData.getCcAddr() == null ? 0L : mailData.getCcAddr().size())
 				+ (mailData.getBccAddr() == null ? 0L : mailData.getBccAddr().size());
