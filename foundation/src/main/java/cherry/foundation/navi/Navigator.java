d6d03722d90df9fd389e7cf3b40327f45baf6566
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-07-04 09:37:48 +0900

    SpringApp: 依存ライブラリ構成
    依存ライブラリ構成見直し。
    * 不要な依存性を排除。

--- foundation/src/main/java/cherry/foundation/navi/Navigator.java
+++ foundation/src/main/java/cherry/foundation/navi/Navigator.java
@@ -18,8 +18,8 @@ package cherry.foundation.navi;
 
 import java.util.List;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 public interface Navigator {
 
