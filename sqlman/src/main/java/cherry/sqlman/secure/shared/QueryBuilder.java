2c9e6de6811a9d50c6d9724796308f40f6c145d7
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-07-05 10:35:26 +0900

    sqlman: 業務ロジック実装

---
+++ sqlman/src/main/java/cherry/sqlman/secure/shared/QueryBuilder.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2014,2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.sqlman.secure.shared;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import org.apache.commons.lang3.StringUtils;
+
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = false)
+public class QueryBuilder {
+
+	private String select = "*";
+
+	private String from = "dual";
+
+	private String where = null;
+
+	private String groupBy = null;
+
+	private String having = null;
+
+	private String orderBy = null;
+
+	public String build() {
+		StringBuilder builder = new StringBuilder();
+		append(builder, "SELECT ", select);
+		append(builder, " FROM ", from);
+		append(builder, " WHERE ", where);
+		append(builder, " GROUP BY ", groupBy);
+		append(builder, " HAVING ", having);
+		append(builder, " ORDER BY ", orderBy);
+		return builder.toString();
+	}
+
+	public String build(Long limit, Long offset) {
+		StringBuilder builder = new StringBuilder();
+		append(builder, "SELECT ", select);
+		append(builder, " FROM ", from);
+		append(builder, " WHERE ", where);
+		append(builder, " GROUP BY ", groupBy);
+		append(builder, " HAVING ", having);
+		append(builder, " ORDER BY ", orderBy);
+		append(builder, " LIMIT ", limit);
+		append(builder, " OFFSET ", offset);
+		return builder.toString();
+	}
+
+	public String buildCount() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("SELECT COUNT(*) FROM (");
+		append(builder, "SELECT ", select);
+		append(builder, " FROM ", from);
+		append(builder, " WHERE ", where);
+		append(builder, " GROUP BY ", groupBy);
+		append(builder, " HAVING ", having);
+		builder.append(") AS A");
+		return builder.toString();
+	}
+
+	private void append(StringBuilder builder, String clause, String value) {
+		if (StringUtils.isNotBlank(value)) {
+			builder.append(clause).append(value);
+		}
+	}
+
+	private void append(StringBuilder builder, String clause, Long value) {
+		if (value != null) {
+			builder.append(clause).append(value);
+		}
+	}
+
+}
---
+++ sqlman/src/main/java/cherry/sqlman/secure/shared/ResultSet.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014,2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.sqlman.secure.shared;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import cherry.foundation.etl.Column;
+import cherry.foundation.etl.Consumer;
+
+@Getter
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = false)
+public class ResultSet implements Consumer {
+
+	private Column[] header;
+
+	private List<Object[]> recordSet = new LinkedList<>();
+
+	@Override
+	public void begin(Column[] col) {
+		header = col;
+	}
+
+	@Override
+	public void consume(Object[] record) {
+		recordSet.add(record);
+	}
+
+	@Override
+	public void end() {
+		// NOTHING TO DO
+	}
+
+}
