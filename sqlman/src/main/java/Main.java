3f26131969888d5e7b5194730cd7bc3cba5269ea
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-07-16 06:08:24 +0900

    sqlman: 実行可能war
    実行可能warを作ってみる。

---
+++ sqlman/src/main/java/Main.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.FragmentConfiguration;
+import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
+import org.eclipse.jetty.xml.XmlConfiguration;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setContextPath("/");
+		webAppContext.setWar(Main.class.getProtectionDomain().getCodeSource().getLocation().toExternalForm());
+		webAppContext.setConfigurations(new Configuration[] { new AnnotationConfiguration(), new WebInfConfiguration(),
+				new WebXmlConfiguration(), new MetaInfConfiguration(), new FragmentConfiguration(),
+				new EnvConfiguration(), new PlusConfiguration(), new JettyWebXmlConfiguration() });
+
+		XmlConfiguration xmlConfig;
+		try (InputStream in = new FileInputStream("jetty-eclipse-config.xml")) {
+			xmlConfig = new XmlConfiguration(in);
+		}
+
+		Server server = new Server(8080);
+		server.setStopAtShutdown(true);
+		xmlConfig.configure(server);
+
+		server.setHandler(webAppContext);
+		server.start();
+		server.join();
+	}
+
+}
