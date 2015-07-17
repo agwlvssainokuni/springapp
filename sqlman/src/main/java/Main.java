41aae8d4dd3ec2b6ff13c9319ebf3e2894e8b75d
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-07-17 20:49:45 +0900

    sqlman: コマンドライン解析
    コマンドラインオプション構成を見直し。

--- sqlman/src/main/java/Main.java
+++ sqlman/src/main/java/Main.java
@@ -39,6 +39,7 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.jetty.webapp.WebInfConfiguration;
 import org.eclipse.jetty.webapp.WebXmlConfiguration;
 import org.h2.jdbcx.JdbcDataSource;
+import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
@@ -51,36 +52,39 @@ public class Main {
 	private static final String DATASOURCE_JNDI_TEMPLATE = "java:/datasources/SqlMan.db{0}";
 	private static final String DATASOURCE_DRIVER = "driver";
 	private static final String DATASOURCE_URL = "url";
-	private static final String DATASOURCE_USER = "user";
+	private static final String DATASOURCE_USERNAME = "username";
 	private static final String DATASOURCE_PASSWORD = "password";
 
-	@Option(name = "-p", aliases = { "--port" }, metaVar = "PORT")
-	private int serverPort = 8080;
+	@Option(name = "-p", aliases = { "--port" }, metaVar = "PORT", usage = "HTTP port")
+	private int httpPort = 8080;
 
-	@Option(name = "-d", aliases = { "--db-url" }, metaVar = "URL")
+	@Option(name = "-d", aliases = { "--url" }, metaVar = "URL", usage = "System DB URL")
 	private String dbUrl = "jdbc:h2:file:./sqlman";
 
-	@Option(name = "-U", aliases = { "--db-user" }, metaVar = "USER")
-	private String dbUser = "sa";
+	@Option(name = "-U", aliases = { "--username" }, metaVar = "USERNAME", usage = "System DB username")
+	private String dbUsername = "sa";
 
-	@Option(name = "-P", aliases = { "--db-password" }, metaVar = "PASSWORD")
+	@Option(name = "-P", aliases = { "--password" }, metaVar = "PASSWORD", usage = "System DB password")
 	private String dbPassword = "";
 
-	@Option(name = "-c", aliases = { "--config" }, metaVar = "CONFIG")
-	private List<File> configFile;
-
-	@Option(name = "-x", aliases = { "--context-path" }, metaVar = "PATH")
+	@Option(name = "-x", aliases = { "--context-path" }, metaVar = "PATH", hidden = true)
 	private String contextPath = "/";
 
-	@Option(name = "-w", aliases = { "--war" }, metaVar = "WAR")
+	@Option(name = "-w", aliases = { "--war" }, metaVar = "WAR", hidden = true)
 	private File warFile = null;
+
+	@Option(name = "-?", aliases = { "--help" }, help = true, hidden = true)
+	private boolean help = false;
+
+	@Argument(multiValued = true, metaVar = "CONFIG...", usage = "DB configuration files")
+	private List<File> configFile;
 
 	public Server prepareServer() throws Exception {
 
-		Server server = new Server(serverPort);
+		Server server = new Server(httpPort);
 		server.setStopAtShutdown(true);
 
-		server.addBean(new Resource(SYSTEM_DATASOURCE_JNDI, createSystemDataSource(dbUrl, dbUser, dbPassword)));
+		server.addBean(new Resource(SYSTEM_DATASOURCE_JNDI, createSystemDataSource(dbUrl, dbUsername, dbPassword)));
 		if (configFile != null) {
 			int count = 1;
 			for (File file : configFile) {
@@ -90,10 +94,10 @@ public class Main {
 				}
 				String driver = props.getProperty(DATASOURCE_DRIVER);
 				String url = props.getProperty(DATASOURCE_URL);
-				String user = props.getProperty(DATASOURCE_USER);
+				String username = props.getProperty(DATASOURCE_USERNAME);
 				String password = props.getProperty(DATASOURCE_PASSWORD);
 				String jndiName = format(DATASOURCE_JNDI_TEMPLATE, count);
-				server.addBean(new Resource(jndiName, createDataSource(driver, url, user, password)));
+				server.addBean(new Resource(jndiName, createDataSource(driver, url, username, password)));
 				count += 1;
 			}
 		}
@@ -151,8 +155,15 @@ public class Main {
 		CmdLineParser parser = new CmdLineParser(this, props);
 		try {
 			parser.parseArgument(args);
+			if (help) {
+				System.err.println(format("java {0} [OPTIONS] [CONFIG...]", Main.class.getName()));
+				parser.printUsage(System.err);
+				return;
+			}
 		} catch (CmdLineException ex) {
+			System.err.println(format("java {0} [OPTIONS] [CONFIG...]", Main.class.getName()));
 			parser.printUsage(System.err);
+			return;
 		}
 
 		startServer();
