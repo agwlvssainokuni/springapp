2f90fee3fe384f5a47c636363bbaded3d175898c
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-07-24 22:17:38 +0900

    SpringApp: 暗号機能
    AES暗号機能を補強。

---
+++ goods/src/main/java/cherry/goods/crypto/AESCrypto.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2014,2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.goods.crypto;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+/**
+ * AES暗号アルゴリズムによる暗号化/復号化の機能を提供する。
+ */
+public class AESCrypto implements Crypto {
+
+	/** AES暗号アルゴリズムを使用するためのアルゴリズム名を保持する。 */
+	private String algorithm = "AES/CBC/PKCS5Padding";
+
+	/** AES暗号アルゴリズムで使用する共通鍵、初期化ベクタを復号化する機能を保持する。 */
+	private Crypto keyCrypto = new NullCrypto();
+
+	/** AES暗号アルゴリズムで使用する共通鍵を保持する。 */
+	private Key secretKey;
+
+	/** AES暗号アルゴリズムで使用する共通鍵の長さを保持する。 */
+	private int secretKeyLength;
+
+	/** AES暗号アルゴリズムで使用する初期化ベクタを生成する。 */
+	private Random random = new SecureRandom();
+
+	public void setAlgorithm(String algorithm) {
+		this.algorithm = algorithm;
+	}
+
+	public void setKeyCrypto(Crypto keyCrypto) {
+		this.keyCrypto = keyCrypto;
+	}
+
+	public void setSecretKeyBytes(byte[] secretKeyBytes) {
+		byte[] keyBin = keyCrypto.decrypt(secretKeyBytes);
+		this.secretKey = KeyUtil.createAesSecretKey(keyBin);
+		this.secretKeyLength = keyBin.length;
+	}
+
+	public void setRandom(Random random) {
+		this.random = random;
+	}
+
+	@Override
+	public byte[] encrypt(byte[] in) {
+		try {
+			byte[] iv = new byte[secretKeyLength];
+			random.nextBytes(iv);
+			Cipher cipher = Cipher.getInstance(algorithm);
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey, KeyUtil.createAesInitVector(iv));
+			byte[] crypto = cipher.doFinal(in);
+			byte[] out = new byte[iv.length + crypto.length];
+			System.arraycopy(iv, 0, out, 0, iv.length);
+			System.arraycopy(crypto, 0, out, iv.length, crypto.length);
+			return out;
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
+				| InvalidKeyException | BadPaddingException | IllegalBlockSizeException ex) {
+			throw new IllegalStateException(ex);
+		}
+	}
+
+	@Override
+	public byte[] decrypt(byte[] in) {
+		try {
+			byte[] iv = new byte[secretKeyLength];
+			byte[] crypto = new byte[in.length - secretKeyLength];
+			System.arraycopy(in, 0, iv, 0, iv.length);
+			System.arraycopy(in, iv.length, crypto, 0, crypto.length);
+			Cipher cipher = Cipher.getInstance(algorithm);
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, KeyUtil.createAesInitVector(iv));
+			return cipher.doFinal(crypto);
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
+				| InvalidKeyException | BadPaddingException | IllegalBlockSizeException ex) {
+			throw new IllegalStateException(ex);
+		}
+	}
+
+}
---
+++ goods/src/test/java/cherry/goods/crypto/AESCryptoTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2014,2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.goods.crypto;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+
+public class AESCryptoTest {
+
+	@Test
+	public void testDefault() throws Exception {
+
+		AESCrypto crypto = new AESCrypto();
+		crypto.setSecretKeyBytes(RandomUtils.nextBytes(16));
+
+		for (int i = 0; i < 100; i++) {
+			byte[] plain = RandomUtils.nextBytes(1024);
+			byte[] enc = crypto.encrypt(plain);
+			byte[] dec = crypto.decrypt(enc);
+			assertThat(dec, is(plain));
+		}
+	}
+
+	@Test
+	public void testCBC() throws Exception {
+
+		AESCrypto crypto = new AESCrypto();
+		crypto.setAlgorithm("AES/CBC/PKCS5Padding");
+		crypto.setSecretKeyBytes(RandomUtils.nextBytes(16));
+
+		for (int i = 0; i < 100; i++) {
+			byte[] plain = RandomUtils.nextBytes(1024);
+			byte[] enc = crypto.encrypt(plain);
+			byte[] dec = crypto.decrypt(enc);
+			assertThat(dec, is(plain));
+		}
+	}
+
+	@Test
+	public void testUsingKeyCrypto() throws Exception {
+
+		byte[] key = RandomUtils.nextBytes(16);
+
+		AESCrypto crypto0 = new AESCrypto();
+		crypto0.setSecretKeyBytes(key);
+
+		AESCrypto keyCrypto = new AESCrypto();
+		keyCrypto.setSecretKeyBytes(RandomUtils.nextBytes(16));
+
+		AESCrypto crypto1 = new AESCrypto();
+		crypto1.setKeyCrypto(keyCrypto);
+		crypto1.setSecretKeyBytes(keyCrypto.encrypt(key));
+
+		for (int i = 0; i < 100; i++) {
+			byte[] plain = RandomUtils.nextBytes(1024);
+			byte[] enc0 = crypto0.encrypt(plain);
+			byte[] enc1 = crypto1.encrypt(plain);
+			assertThat(enc1, is(not(enc0)));
+			byte[] dec0 = crypto0.decrypt(enc0);
+			byte[] dec1 = crypto1.decrypt(enc1);
+			assertThat(dec0, is(plain));
+			assertThat(dec1, is(plain));
+		}
+	}
+
+}
