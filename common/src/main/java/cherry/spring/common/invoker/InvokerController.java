dac07e63b02acc71a2cc331a25003004b17ce53d
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-08-01 11:14:47 +0900

    common: 汎用起動機構

---
+++ common/src/main/java/cherry/spring/common/invoker/InvokerController.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.spring.common.invoker;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/secure/invoker")
+public interface InvokerController {
+
+	@RequestMapping()
+	@ResponseBody()
+	String invoke(@RequestParam(value = "beanName", required = false) String beanName,
+			@RequestParam("className") String className, @RequestParam("methodName") String methodName,
+			@RequestParam(value = "methodIndex", defaultValue = "0") int methodIndex,
+			@RequestParam("args") List<String> args);
+
+}
---
+++ common/src/main/java/cherry/spring/common/invoker/InvokerControllerImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.spring.common.invoker;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+import cherry.foundation.invoker.Invoker;
+import cherry.goods.util.ToMapUtil;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Controller
+public class InvokerControllerImpl implements InvokerController {
+
+	@Autowired
+	private Invoker invoker;
+
+	@Autowired
+	@Qualifier("objectMapper")
+	private ObjectMapper objectMapper;
+
+	@Override
+	public String invoke(String beanName, String className, String methodName, int methodIndex, List<String> args) {
+		try {
+			return invoker.invoke(beanName, className, methodName, methodIndex, args.toArray(new String[args.size()]));
+		} catch (ClassNotFoundException | NoSuchMethodException ex) {
+			return fromThrowableToString(ex);
+		} catch (IllegalStateException ex) {
+			return fromThrowableToString(ex.getCause());
+		}
+	}
+
+	private String fromThrowableToString(Throwable ex) {
+		Map<String, ?> map = ToMapUtil.fromThrowable(ex, Integer.MAX_VALUE);
+		try {
+			return objectMapper.writeValueAsString(map);
+		} catch (IOException ex2) {
+			return ex.getMessage();
+		}
+	}
+
+}
