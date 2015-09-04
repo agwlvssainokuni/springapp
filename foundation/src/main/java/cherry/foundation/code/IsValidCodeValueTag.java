c24af19a769bde4da543d5fab794715caee54fc9
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-09-05 07:53:04 +0900

    foundation: 区分値管理機能
    JSP拡張タグ。

---
+++ foundation/src/main/java/cherry/foundation/code/IsValidCodeValueTag.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2015 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.foundation.code;
+
+public class IsValidCodeValueTag extends CodeTagSupport<Boolean> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected Boolean getObject(CodeManager codeManager, String codeName, String value, Boolean plainLabel) {
+		return codeManager.isValidValue(codeName, value);
+	}
+
+	@Override
+	protected boolean doesEvalBody(CodeManager codeManager, String codeName, String value, Boolean plainLabel) {
+		return codeManager.isValidValue(codeName, value);
+	}
+
+}
