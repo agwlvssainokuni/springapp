cc94d6c2e1e6d71dedfc5080ead5cdf461f68278
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-11-21 23:26:31 +0900

    SQL管理ツール

--- sqlman/src/main/java/cherry/sqlman/tool/clause/SqlClauseControllerImpl.java
+++ sqlman/src/main/java/cherry/sqlman/tool/clause/SqlClauseControllerImpl.java
@@ -16,14 +16,19 @@
 
 package cherry.sqlman.tool.clause;
 
+import static cherry.foundation.springmvc.Contract.shouldExist;
+import static cherry.sqlman.ParamDef.REQ_ID;
+import static cherry.sqlman.ParamDef.REQ_REF;
+import static cherry.sqlman.util.ModelAndViewBuilder.redirect;
+import static cherry.sqlman.util.ModelAndViewBuilder.withViewname;
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.fromMethodCall;
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
 
 import java.util.Locale;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.BadSqlGrammarException;
@@ -31,19 +36,20 @@ import org.springframework.mobile.device.site.SitePreference;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import cherry.foundation.logicalerror.LogicalErrorUtil;
 import cherry.goods.paginate.PageSet;
 import cherry.sqlman.LogicError;
-import cherry.sqlman.PathDef;
 import cherry.sqlman.tool.metadata.MetadataService;
 import cherry.sqlman.tool.metadata.SqlMetadataForm;
 import cherry.sqlman.tool.shared.DataSourceDef;
 import cherry.sqlman.tool.shared.ResultSet;
 import cherry.sqlman.tool.util.Util;
+import cherry.sqlman.util.ViewNameUtil;
 
 @Controller
 public class SqlClauseControllerImpl extends SqlClauseSupport implements SqlClauseController {
@@ -57,59 +63,45 @@ public class SqlClauseControllerImpl extends SqlClauseSupport implements SqlClau
 	@Autowired
 	private DataSourceDef dataSourceDef;
 
+	private final String viewnameOfStart = ViewNameUtil.fromMethodCall(on(SqlClauseController.class).start(null, null,
+			null, null, null, null, null));
+
 	@Override
-	public SqlClauseForm getForm(Integer ref, Authentication auth) {
-		if (ref != null) {
-			SqlMetadataForm mdForm = metadataService.findById(ref, auth.getName());
-			if (mdForm != null) {
-				SqlClauseForm form = clauseService.findById(ref);
-				if (form != null) {
-					return form;
-				}
-			}
-		}
-		SqlClauseForm form = new SqlClauseForm();
-		form.setDatabaseName(dataSourceDef.getDefaultName());
-		return form;
+	public ModelAndView init(String redirTo, Integer ref, Authentication auth, Locale locale, SitePreference sitePref,
+			NativeWebRequest request) {
+		return redirect(redirectOnInit(redirTo, ref)).build();
 	}
 
 	@Override
-	public ModelAndView init(Authentication auth, Locale locale, SitePreference sitePref, HttpServletRequest request) {
-		ModelAndView mav = new ModelAndView(PathDef.VIEW_TOOL_CLAUSE);
-		return mav;
+	public ModelAndView start(Integer ref, SqlClauseForm form, BindingResult binding, Authentication auth,
+			Locale locale, SitePreference sitePref, NativeWebRequest request) {
+		initializeForm(form, ref, auth);
+		return withViewname(viewnameOfStart).build();
 	}
 
 	@Override
 	public ModelAndView execute(SqlClauseForm form, BindingResult binding, Authentication auth, Locale locale,
-			SitePreference sitePref, HttpServletRequest request) {
+			SitePreference sitePref, NativeWebRequest request) {
 
-		if (binding.hasErrors()) {
-			ModelAndView mav = new ModelAndView(PathDef.VIEW_TOOL_CLAUSE);
-			return mav;
+		if (hasErrors(form, binding)) {
+			return withViewname(viewnameOfStart).build();
 		}
 
 		try {
-
 			Pair<PageSet, ResultSet> pair = search(form);
-
-			ModelAndView mav = new ModelAndView(PathDef.VIEW_TOOL_CLAUSE);
-			mav.addObject(pair.getLeft());
-			mav.addObject(pair.getRight());
-			return mav;
+			return withViewname(viewnameOfStart).addObject(pair.getLeft()).addObject(pair.getRight()).build();
 		} catch (BadSqlGrammarException ex) {
 			LogicalErrorUtil.reject(binding, LogicError.BadSqlGrammer, Util.getRootCause(ex).getMessage());
-			ModelAndView mav = new ModelAndView(PathDef.VIEW_TOOL_CLAUSE);
-			return mav;
+			return withViewname(viewnameOfStart).build();
 		}
 	}
 
 	@Override
 	public ModelAndView download(SqlClauseForm form, BindingResult binding, Authentication auth, Locale locale,
-			SitePreference sitePref, HttpServletRequest request, HttpServletResponse response) {
+			SitePreference sitePref, NativeWebRequest request, HttpServletResponse response) {
 
-		if (binding.hasErrors()) {
-			ModelAndView mav = new ModelAndView(PathDef.VIEW_TOOL_CLAUSE);
-			return mav;
+		if (hasErrors(form, binding)) {
+			return withViewname(viewnameOfStart).build();
 		}
 
 		try {
@@ -117,27 +109,74 @@ public class SqlClauseControllerImpl extends SqlClauseSupport implements SqlClau
 			return null;
 		} catch (BadSqlGrammarException ex) {
 			LogicalErrorUtil.reject(binding, LogicError.BadSqlGrammer, Util.getRootCause(ex).getMessage());
-			ModelAndView mav = new ModelAndView(PathDef.VIEW_TOOL_CLAUSE);
-			return mav;
+			return withViewname(viewnameOfStart).build();
 		}
 	}
 
 	@Override
 	public ModelAndView create(SqlClauseForm form, BindingResult binding, Authentication auth, Locale locale,
-			SitePreference sitePref, HttpServletRequest request) {
+			SitePreference sitePref, NativeWebRequest request) {
 
-		if (binding.hasErrors()) {
-			ModelAndView mav = new ModelAndView(PathDef.VIEW_TOOL_CLAUSE);
-			return mav;
+		if (hasErrors(form, binding)) {
+			return withViewname(viewnameOfStart).build();
 		}
 
 		int id = clauseService.create(form, auth.getName());
+		return redirect(redirectOnCreate(id)).build();
+	}
 
-		UriComponents uc = fromMethodCall(on(SqlClauseIdController.class).init(id, auth, locale, sitePref, request))
-				.build();
-		ModelAndView mav = new ModelAndView();
-		mav.setView(new RedirectView(uc.toUriString(), true));
-		return mav;
+	private UriComponents redirectOnInit(String redir, Integer ref) {
+		if (StringUtils.isNotEmpty(redir)) {
+			return UriComponentsBuilder.fromPath(redir).build();
+		} else {
+			UriComponentsBuilder ucb = fromMethodCall(on(SqlClauseController.class).start(null, null, null, null, null,
+					null, null));
+			if (ref == null) {
+				return ucb.replaceQuery(StringUtils.EMPTY).build();
+			} else {
+				return ucb.replaceQueryParam(REQ_REF, ref).build();
+			}
+		}
+	}
+
+	private UriComponents redirectOnCreate(int id) {
+		return fromMethodCall(on(SqlClauseIdController.class).init(id, null, null, null, null)).replaceQueryParam(
+				REQ_ID, id).build();
+	}
+
+	private void initializeForm(SqlClauseForm form, Integer ref, Authentication auth) {
+		if (ref == null) {
+			form.setDatabaseName(dataSourceDef.getDefaultName());
+			return;
+		}
+		SqlMetadataForm mdForm = metadataService.findById(ref, auth.getName());
+		if (mdForm != null) {
+			SqlClauseForm f = clauseService.findById(ref);
+			shouldExist(f, SqlClauseForm.class, ref);
+			form.setDatabaseName(f.getDatabaseName());
+			form.setSelect(f.getSelect());
+			form.setFrom(f.getFrom());
+			form.setWhere(f.getWhere());
+			form.setGroupBy(f.getGroupBy());
+			form.setHaving(f.getHaving());
+			form.setOrderBy(f.getOrderBy());
+			form.setParamMap(f.getParamMap());
+			form.setLockVersion(f.getLockVersion());
+		}
+	}
+
+	private boolean hasErrors(SqlClauseForm form, BindingResult binding) {
+
+		// 単項目チェック
+		if (binding.hasErrors()) {
+			return true;
+		}
+
+		// 項目間チェック
+
+		// 整合性チェック
+
+		return false;
 	}
 
 }
---
+++ sqlman/src/main/webapp/WEB-INF/tiles/normal/tool/clause/startNew.jsp
@@ -0,0 +1,157 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" trimDirectiveWhitespaces="true" session="false"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="f" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags"%>
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
+<%@ taglib prefix="app" tagdir="/WEB-INF/tags"%>
+<%@ taglib prefix="foundation" uri="urn:cherry:foundation"%>
+<s:url var="baseUri" value="/tool/clause" />
+<c:set var="hasResultList" value="${resultSet != null && pageSet != null}" />
+<foundation:getBean var="dataSourceDef" beanTypeName="cherry.sqlman.tool.shared.DataSourceDef" />
+<h2 class="page-header">句指定SQL実行</h2>
+<div class="panel-group">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">
+				<a data-toggle="collapse" href="#clauseForm">SQL指定</a>
+			</h3>
+		</div>
+		<div id="clauseForm" class="panel-collapse collapse ${hasResultList ? '' : 'in'}">
+			<div class="panel-body">
+				<s:hasBindErrors name="sqlClauseForm">
+					<div class="col-sm-offset-2 col-sm-10">
+						<div class="alert alert-danger" role="alert">
+							<f:errors path="sqlClauseForm" element="div" />
+							<f:errors path="sqlClauseForm.databaseName" element="div" />
+							<f:errors path="sqlClauseForm.select" element="div" />
+							<f:errors path="sqlClauseForm.from" element="div" />
+							<f:errors path="sqlClauseForm.where" element="div" />
+							<f:errors path="sqlClauseForm.groupBy" element="div" />
+							<f:errors path="sqlClauseForm.having" element="div" />
+							<f:errors path="sqlClauseForm.orderBy" element="div" />
+							<f:errors path="sqlClauseForm.paramMap" element="div" />
+							<f:errors path="sqlClauseForm.lockVersion" element="div" />
+						</div>
+					</div>
+				</s:hasBindErrors>
+				<f:form servletRelativeAction="${baseUri}/executeNew" method="POST" modelAttribute="sqlClauseForm" cssClass="form-horizontal" role="form">
+					<f:hidden path="pageNo" value="0" />
+					<f:hidden path="pageSz" />
+					<f:hidden path="lockVersion" />
+					<div class="form-group">
+						<div>
+							<f:label path="databaseName" cssClass="col-sm-2 control-label" cssErrorClass="col-sm-2 control-label has-error">DB名称</f:label>
+						</div>
+						<div class="col-sm-10">
+							<f:select path="databaseName" cssClass="form-control" cssErrorClass="form-control has-error">
+								<f:options items="${dataSourceDef.names}" />
+							</f:select>
+						</div>
+					</div>
+					<div class="form-group">
+						<div>
+							<f:label path="select" cssClass="col-sm-2 control-label" cssErrorClass="col-sm-2 control-label has-error">SELECT</f:label>
+						</div>
+						<div class="col-sm-10">
+							<f:textarea path="select" cssClass="form-control" cssErrorClass="form-control has-error" />
+						</div>
+					</div>
+					<div class="form-group">
+						<div>
+							<f:label path="from" cssClass="col-sm-2 control-label" cssErrorClass="col-sm-2 control-label has-error">FROM</f:label>
+						</div>
+						<div class="col-sm-10">
+							<f:textarea path="from" cssClass="form-control" cssErrorClass="form-control has-error" />
+						</div>
+					</div>
+					<div class="form-group">
+						<div>
+							<f:label path="where" cssClass="col-sm-2 control-label" cssErrorClass="col-sm-2 control-label has-error">WHERE</f:label>
+						</div>
+						<div class="col-sm-10">
+							<f:textarea path="where" cssClass="form-control" cssErrorClass="form-control has-error" />
+						</div>
+					</div>
+					<div class="form-group">
+						<div>
+							<f:label path="groupBy" cssClass="col-sm-2 control-label" cssErrorClass="col-sm-2 control-label has-error">GROUP BY</f:label>
+						</div>
+						<div class="col-sm-10">
+							<f:textarea path="groupBy" cssClass="form-control" cssErrorClass="form-control has-error" />
+						</div>
+					</div>
+					<div class="form-group">
+						<div>
+							<f:label path="having" cssClass="col-sm-2 control-label" cssErrorClass="col-sm-2 control-label has-error">HAVING</f:label>
+						</div>
+						<div class="col-sm-10">
+							<f:textarea path="having" cssClass="form-control" cssErrorClass="form-control has-error" />
+						</div>
+					</div>
+					<div class="form-group">
+						<div>
+							<f:label path="orderBy" cssClass="col-sm-2 control-label" cssErrorClass="col-sm-2 control-label has-error">ORDER BY</f:label>
+						</div>
+						<div class="col-sm-10">
+							<f:textarea path="orderBy" cssClass="form-control" cssErrorClass="form-control has-error" />
+						</div>
+					</div>
+					<div class="form-group">
+						<f:label path="paramMap" cssClass="col-sm-2 control-label" cssErrorClass="col-sm-2 control-label has-error">埋込パラメタ</f:label>
+						<div class="col-sm-10">
+							<f:textarea path="paramMap" cssClass="form-control" cssErrorClass="form-control has-error" />
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-offset-2 col-sm-10">
+							<f:button type="submit" class="btn btn-primary">SQL実行</f:button>
+							<f:button type="submit" name="download" class="btn btn-default">ダウンロード</f:button>
+							<f:button type="submit" name="create" class="btn btn-default">SQL登録</f:button>
+						</div>
+					</div>
+				</f:form>
+			</div>
+		</div>
+	</div>
+	<c:if test="${hasResultList}">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					<a data-toggle="collapse" href="#clauseResult">実行結果</a>
+				</h3>
+			</div>
+			<div id="clauseResult" class="panel-collapse collapse in">
+				<f:form servletRelativeAction="${baseUri}/executeNew" method="POST" modelAttribute="sqlClauseForm" id="sqlClause2">
+					<f:hidden id="pageNo2" path="pageNo" />
+					<f:hidden id="pageSz2" path="pageSz" />
+					<f:hidden id="lockVersion2" path="lockVersion" />
+					<f:hidden id="databaseName2" path="databaseName" />
+					<f:hidden id="select2" path="select" />
+					<f:hidden id="from2" path="from" />
+					<f:hidden id="where2" path="where" />
+					<f:hidden id="groupBy2" path="groupBy" />
+					<f:hidden id="having2" path="having" />
+					<f:hidden id="orderBy2" path="orderBy" />
+					<f:hidden id="paramMap2" path="paramMap" />
+				</f:form>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-sm-offset-6 col-sm-6 text-right">
+							<app:pagerDesc pageSet="${pageSet}" />
+							<app:pagerLink pageSet="${pageSet}" form="#sqlClause2" pno="pageNo" />
+						</div>
+					</div>
+					<app:resultSet id="resultSetList" resultSet="${resultSet}" pageSet="${pageSet}" />
+					<div class="row">
+						<div class="col-sm-offset-6 col-sm-6 text-right">
+							<app:pagerLink pageSet="${pageSet}" form="#sqlClause2" pno="pageNo" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</c:if>
+</div>
